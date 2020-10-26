09edf6fe239239dcb830e344ce0d81d0133110fd
theodorebrinkofski <tgb29@cornell.edu>
2020-10-26 19:20:50 -0400

    take_survey

--- src/profile.py
+++ src/profile.py
@@ -4,12 +4,12 @@ from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key
 
 
-def load_profile(user_id):
-    profile_table_name = os.environ["PROFILE_TABLE"]
+def loadProfile(user_id):
+    profileTableName = os.environ["PROFILE_TABLE"]
     dynamodb = boto3.resource('dynamodb')
-    profile_table = dynamodb.Table(profile_table_name)
+    profileTable = dynamodb.Table(profileTableName)
     try:
-        response = profile_table.query(
+        response = profileTable.query(
             KeyConditionExpression=Key("UserId").eq(user_id),
         )
     except ClientError as e:
@@ -19,12 +19,6 @@ def load_profile(user_id):
             'profile': 'Invalid user_id'
         }
     else:
-        '''
-        if "balance" in response['Items'][0].keys():
-            balance = response['Items'][0]["balance"]
-        else:
-            balance = "0"
-        '''
         if "currency" in response['Items'][0].keys():
             currency = response['Items'][0]["currency"]
         else:
@@ -43,7 +37,6 @@ def load_profile(user_id):
             "email": response['Items'][0]["Email"],
             "signupDate": response['Items'][0]["CreatedAt"],
             "UserID": response['Items'][0]["UserId"],
-            #  "balance": balance,
             "currency": currency,
             "lang": lang,
             "gravatarEmail": ge
@@ -54,43 +47,20 @@ def load_profile(user_id):
         }
 
 
-def load_history(user_id):
-    ledger_table_name = os.environ["LEDGER_TABLE"]
+def loadHistory(user_id):
+    ledgerTableName = os.environ["LEDGER_TABLE"]
     dynamodb = boto3.resource('dynamodb')
-    ledger_table = dynamodb.Table(ledger_table_name)
-
-    #  transaction_table_name = os.environ["TRANSACTION_TABLE"]
-    #  transaction_table = dynamodb.Table(transaction_table_name)
-
+    ledgerTable = dynamodb.Table(ledgerTableName)
     try:
-        ledger_history = ledger_table.query(
+        ledgerHistory = ledgerTable.query(
             KeyConditionExpression=Key("UserId").eq(user_id),
             ScanIndexForward=False)
-        '''
-        transaction_history = transaction_table.query(
-            KeyConditionExpression=Key("UserId").eq(user_id),
-            ScanIndexForward=False)
-
-        history = mergeHistory(ledger_history, transaction_history)
-        '''
-
-        history = ledger_history["Items"]
+        history = ledgerHistory["Items"]
     except ClientError as e:
         print("Failed to query ledger for userId=%s error=%s", user_id, e.response['Error']['Message'])
         return 'error', {}
     else:
-        return 'success', history  # json.dumps(ledger_history['Items'])
-
-
-'''
-def mergeHistory(ledger_history, transaction_history):
-    history_list = []
-    for i in ledger_history['Items']:
-        history_list.append(i)
-    for k in transaction_history['Items']:
-        history_list.append(k)
-    return history_list
-'''
+        return 'success', history
 
 
 def getBalance(history):
@@ -108,39 +78,39 @@ def getBalance(history):
         return str(debit)
 
 
-def get_survey_object(userId):
-    config_table_name = os.environ["CONFIG_TABLE"]
-    config_key = "TakeSurveyPage"
+def getSurveyObject(userId):
+    configTableName = os.environ["CONFIG_TABLE"]
+    configKey = "TakeSurveyPage"
     dynamodb = boto3.resource('dynamodb')
-    config_table = dynamodb.Table(config_table_name)
+    configTable = dynamodb.Table(configTableName)
     URL = "https://cesyiqf0x6.execute-api.us-west-2.amazonaws.com/prod/SudoCoinsTakeSurvey?ip=108.50.251.254"
     try:
-        response = config_table.get_item(Key={'configKey': config_key})
+        response = configTable.get_item(Key={'configKey': configKey})
     except ClientError as e:
         print("Failed to query config error=%s", e.response['Error']['Message'])
         return None
     else:
-        config_data = response['Item']
-        buyer_object = []
-        for i in config_data['configValue']['publicBuyers']:
-            buyer_object.append(config_data['configValue']["buyer"][i])
-        survey_tiles = []
-        for i in buyer_object:
+        configData = response['Item']
+        buyerObject = []
+        for i in configData['configValue']['publicBuyers']:
+            buyerObject.append(configData['configValue']["buyer"][i])
+        surveyTiles = []
+        for i in buyerObject:
             buyer = {
                 "name": i["name"],
                 "iconLocation": i["iconLocation"],
                 "incentive": i["defaultCPI"],
                 "URL": URL + "&buyer_name=" + i["name"] + "&user_id=" + userId
             }
-            survey_tiles.append(buyer)
-        return survey_tiles
+            surveyTiles.append(buyer)
+        return surveyTiles
 
 
 def lambda_handler(event, context):
     print("event=%s userId=%", event, context.identity.cognito_identity_id)
-    json_input = event
-    profile_resp = load_profile(json_input["user_id"])
-    if profile_resp["statusCode"] != 200:
+    jsonInput = event
+    profileResp = loadProfile(jsonInput["user_id"])
+    if profileResp["statusCode"] != 200:
         return {
             'statusCode': 400,
             'body': {
@@ -148,23 +118,15 @@ def lambda_handler(event, context):
                 "error": 'Invalid account'
             }
         }
-    history_status, history = load_history(json_input["user_id"])
-    if history_status != 'success':
-        return {
-            'statusCode': 400,
-            'body': {
-                "code": 2,
-                "profile": profile_resp["profile"],
-                "history": history
-            }
-        }
-    print(history)
-    profile_resp["profile"]["balance"] = getBalance(history)
-    survey_tile = get_survey_object(json_input["user_id"])
-    if survey_tile is None:
+    historyStatus, history = loadHistory(jsonInput["user_id"])
+
+    profileResp["profile"]["balance"] = getBalance(history)
+
+    surveyTile = getSurveyObject(jsonInput["user_id"])
+    if surveyTile is None:
         data = {
             "code": 3,
-            "profile": profile_resp["profile"],
+            "profile": profileResp["profile"],
             "history": history,
             "survey_tile": "Error fetching survey tile"
         }
@@ -175,8 +137,8 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': {
-            "profile": profile_resp["profile"],
+            "profile": profileResp["profile"],
             "history": history,
-            "survey": survey_tile
+            "survey": surveyTile
         }
     }
