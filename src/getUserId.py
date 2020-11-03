60173247e6efd8993c074606b75f42f1b01a5644
theodorebrinkofski <tgb29@cornell.edu>
2020-11-02 22:05:14 -0500

    setProfile and getUserId for Cognito integration

--- src/getUserId.py
+++ src/getUserId.py
@@ -5,10 +5,43 @@ from datetime import datetime
 
 
 def lambda_handler(event, context):
-
+    dynamodb = boto3.resource('dynamodb')
+    profileTable = dynamodb.Table("Profile")
     print(event)
 
-    return {
-        'statusCode': 200,
-        'body': "Profile saved"
-    }
+    userData = event["request"]["userAttributes"]
+
+    # Get userId from email index
+    if 'email' in userData.keys():
+        email = userData["email"]
+
+        response = profileTable.query(
+            IndexName='email-index',
+            KeyConditionExpression='email = :email',
+            ExpressionAttributeValues={
+                ':email': email
+            })
+    # if an email is found, use the userId associated with it.
+    if response['Count'] > 0:
+        return response['Items']['userId']
+
+    else:
+        #  if user signed up with phone number
+        return {
+            'statusCode': 200,
+            'body': userData["sub"]
+        }
+
+    '''
+    {'userName': 'tedbrink29@gmail.com',
+     'request': {
+        'userAttributes': {
+            'sub': '58ed289b-0bb1-4741-aa9f-3802262d319a',
+            'email_verified': 'true',
+            'cognito:user_status': 'CONFIRMED',
+            'phone_number_verified': 'false',
+            'phone_number': '+17329938083',
+            'email': 'tedbrink29@gmail.com'}
+        }
+     }
+    '''
--- src/setProfile.py
+++ src/setProfile.py
@@ -5,10 +5,55 @@ from datetime import datetime
 
 
 def lambda_handler(event, context):
-
+    dynamodb = boto3.resource('dynamodb')
+    profileTable = dynamodb.Table("Profile")
     print(event)
 
-    return {
-        'statusCode': 200,
-        'body': "Profile saved"
-    }
+    ts = str(datetime.utcnow().isoformat())
+    userData = event["request"]["userAttributes"]
+    # Check to see if the registering user has an already registered email
+    if 'email' in userData.keys():
+        email = userData["email"]
+
+        response = profileTable.query(
+            IndexName='email-index',
+            KeyConditionExpression='email = :email',
+            ExpressionAttributeValues={
+                ':email': email
+            })
+    # if an email is found, use the existing userId
+    if response['Count'] > 0:
+        return response['Items']['userId']
+
+    else:
+        userProfile = {}
+        for i in userData:
+            userProfile[i] = userData[i]
+
+        if 'sub' in userProfile:
+            userProfile["userId"] = userProfile['sub']
+        else:
+            userProfile["userId"] = str(uuid.uuid1())
+
+            userProfile['createdAt'] = ts
+            userProfile['identityProvider'] = 'Cognito'
+            userProfile['status'] = True
+
+            return {
+                'statusCode': 200,
+                'body': userProfile["userId"]
+            }
+
+    '''
+    {'userName': 'tedbrink29@gmail.com',
+     'request': {
+        'userAttributes': {
+            'sub': '58ed289b-0bb1-4741-aa9f-3802262d319a',
+            'email_verified': 'true',
+            'cognito:user_status': 'CONFIRMED',
+            'phone_number_verified': 'false',
+            'phone_number': '+17329938083',
+            'email': 'tedbrink29@gmail.com'}
+        }
+     }
+    '''
