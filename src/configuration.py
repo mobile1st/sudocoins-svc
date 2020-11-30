d84b48638049a38e36c201ded978c81e598f06b9
vercy <megavercy@gmail.com>
2020-11-30 10:26:27 +0900

    create only one connection pool in profile lambda + testing configuration reader

--- src/configuration.py
+++ src/configuration.py
@@ -2,19 +2,49 @@
 
 class Configuration:
 
-    def __init__(self, dynamodb, cache):
+    def __init__(self, dynamodb, cache=None):
         self.dynamodb = dynamodb
         self.cache = cache
+        self.take_survey_config = None
 
-    def get_buyer(self, name):
-        # todo 1: look in filesystem 2: load from dynamodb
-        buyer = self.load_buyer(name)
-        return BuyerConfiguration(buyer)
+    def buyer(self, name):
+        buyers = self._get_take_survey_key()
+        return buyers[name]
 
-    def load_buyer(self, name):
-        configTable = self.dynamodb.Table("Config")
-        buyerObject = configTable.get_item(Key={'configKey': 'TakeSurveyPage'})
-        return buyerObject["Item"]["configValue"]["buyer"][name]
+    def public_buyers(self):
+        _, public_buyers = self._get_take_survey_key()
+        return public_buyers
+
+    def _get_take_survey_key(self):
+        print('Configuration read=take_survey')
+        if self.take_survey_config is not None:
+            print('Configuration found in memory')
+            return self.take_survey_config
+
+        buyers, public_buyers = self._load_take_survey_key_disk()
+        if buyers is not None and public_buyers is not None:
+            self.take_survey_config = buyers, public_buyers
+            print('Configuration loaded form disk')
+            return self.take_survey_config
+
+        print('Configuration not on disk, loading from database')
+        self.take_survey_config = self._load_take_survey_key_db()
+        print('Configuration loaded form database')
+        return self.take_survey_config
+
+    def _load_take_survey_key_disk(self):
+        # todo using self.cache
+        return None, None
+
+    def _load_take_survey_key_db(self):
+        config = self.dynamodb.Table("Config")
+        row = config.get_item(Key={'configKey': 'TakeSurveyPage'})
+        take_survey = row["Item"]["configValue"]
+        buyers = {}
+        for name, cfg in take_survey["buyer"]:
+            buyers[name] = BuyerConfiguration(cfg)
+
+        return buyers, take_survey["publicBuyers"]
 
 
 class BuyerConfiguration:
