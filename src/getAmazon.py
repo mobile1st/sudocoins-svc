77ee5948a7b9161d56e1b3dbc32e0ebc567277f9
theodorebrinkofski <tgb29@cornell.edu>
2020-12-01 18:15:01 -0500

    getAmazon final edits, now working

--- src/getAmazon.py
+++ src/getAmazon.py
@@ -1,49 +1,76 @@
-import requests
 from bs4 import BeautifulSoup
-import random
 from scraper_api import ScraperAPIClient
-import lxml
+import datetime
 
 
 def lambda_handler(event, context):
     url = event['url']
+    print("cleaning url")
+    print(datetime.datetime.now())
+    print("")
+
     cleanUrl = parseUrl(url)
-    '''
-    client = ScraperAPIClient('23697b4011e85f37992063c81f9e03ff')
-    result = client.get(url = 'https://www.amazon.com/dp/B087CSW37P', country_code = "US").text
-    soup = BeautifulSoup(result, "html.parser")
-    price = soup.find("div", class_="a-section").find("span", class_="a-size-medium a-color-price")
-    '''
-    price, title, rating, imgUrl = loadAmazon(cleanUrl)
+
+    print('url cleaned')
+    print(datetime.datetime.now())
+    print("")
+
+    print("calling loadAmazon")
+    print(datetime.datetime.now())
+    print("")
+
+    price, title = loadAmazon(cleanUrl)  # , rating, imgUrl
+
+    print("done parsing")
+    print(datetime.datetime.now())
+    print("")
 
     return {
         'statusCode': 200,
         'body': {
-            # "price": price,
-            # "title": title,
+            "price": price,
+            "title": title  # ,
             # "rating": rating,
             # "imgUrl": imgUrl
-            "price": price.text
+
         }
     }
 
 
 def loadAmazon(url):
-    #  need process to change headers dynamically to avoid bot detection
-    headers = ({'User-Agent':
-                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                    '(KHTML, like Gecko) Chrome/44.0.2403.157 '
-                    'Safari/537.36',
-                'Accept-Language': 'en-US, en;q=0.5'})
-    page = requests.get(url, headers=headers)
+    print("creating client")
+    print(datetime.datetime.now())
+    print("")
+
+    client = ScraperAPIClient('23697b4011e85f37992063c81f9e03ff')
+
+    print("client created")
+    print(datetime.datetime.now())
+    print("")
+
+    print("calling scraper")
+    print(datetime.datetime.now())
+    print("")
+
+    page = client.get(url=url, country_code="US")
+
+    print("receive response")
+    print(datetime.datetime.now())
+    print("")
+
     soup = BeautifulSoup(page.text, "lxml")
+
+    print("souped")
+    print(datetime.datetime.now())
+    print("")
+
     price = soup.find("span", attrs={'id': 'price_inside_buybox'}).string.strip()
     title = soup.find("span", attrs={'id': 'productTitle'}).string.strip()
-    rating = soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip()
-    img = soup.find(id="imgTagWrapperId")
-    imgUrl = img.findChild("img")['data-a-dynamic-image'].split(',')[0].split('"')[1]
+    # rating = "" #soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip()
+    # imgUrl = "" #soup.find(id="imgTagWrapperId").findChild("img")['data-a-dynamic-image'].split(',')[0].split('"')[1]
+    # imgUrl = img.findChild("img")['data-a-dynamic-image'].split(',')[0].split('"')[1]
 
-    return price, title, rating, imgUrl
+    return price, title  # , rating, imgUrl
 
 
 def parseUrl(url):
