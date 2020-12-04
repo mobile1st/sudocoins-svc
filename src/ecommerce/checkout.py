f5d31fc91db0748972404bbf8cad2c5e4f978f44
theodorebrinkofski <tgb29@cornell.edu>
2020-12-03 20:37:49 -0500

    add code for checkoutLambda

--- src/ecommerce/checkout.py
+++ src/ecommerce/checkout.py
@@ -7,6 +7,69 @@ from decimal import *
 def lambda_handler(event, context):
     print(event)
 
+    try:
+
+        if 'sub' in event:
+            sub = event['sub']
+            userId = getUserId(sub)
+            if 'sourceIp' in event['requestContext']['identity']:
+                ip = 'sourceIp' in event['requestContext']['identity']
+        else:
+            if 'sourceIp' in event['requestContext']['identity']:
+                userId = event['requestContext']['identity']['sourceIp']
+                ip = event['requestContext']['identity']['sourceIp']
+            else:
+                userId = str(uuid.uuid1())
+                ip = ""
+
+            orderId = str(uuid.uuid1())
+
+            wyreBody = {
+                "userId": userId,
+                "orderId": orderId,
+                "currency": event['body']['currency']
+            }
+
+            # methond to Wyre queue to create wallet
+
+            created = str(datetime.utcnow().isoformat())
+
+            orderRecord = {
+                "userId": userId,
+                "orderId": orderId,
+                "statusCode": 2,
+                "statusMessage": "Payment needed",
+                "created": created,
+                "started": created,
+                "end": created + datetime.timedelta(minutes=15),
+                "amountUsd": event['body']['amountUsd'],
+                "amountCurrency": event['body']['amountCurrency'],  # or we calculate
+                "currency": event['body']['amountCurrency'],
+                "rate": event['body']['rate'],
+                "shippingState": "false",
+                "shippingAddress": ""
+
+            }
+
+            # put order record in DB
+
+            response = {
+                "statusCode": 200,
+                "body": "success"
+            }
+
+            return response
+
+
+    except Exception as e:
+        print(e)
+        response = {
+            "statusCode": 200,
+            "body": "error"
+        }
+
+        return response
+
 
 def getUserId(sub):
     dynamodb = boto3.resource('dynamodb')
@@ -15,5 +78,6 @@ def getUserId(sub):
 
     if 'Item' in subResponse:
         print("founder userId matching sub")
-
         userId = subResponse['Item']['userId']
+
+        return userId
