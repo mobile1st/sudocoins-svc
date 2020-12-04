d719f9b601bc4917ad6016868735a95a87bab88a
theodorebrinkofski <tgb29@cornell.edu>
2020-12-04 18:11:42 -0500

    add setShipping and wyreCallback code

--- src/ecommerce/wyreCallback.py
+++ src/ecommerce/wyreCallback.py
@@ -6,3 +6,30 @@ from decimal import *
 
 def lambda_handler(event, context):
     print(event)
+
+    # parse event. Will it provide walletId or srn? Do we need to do a lookup?
+    # save status
+    # updateOrder(status)
+
+    return {
+        "statusCode": 200,
+        "body": "success"
+    }
+
+
+def updateOrder(status):
+    dynamodb = boto3.resource('dynamodb')
+    ordersTable = dynamodb.Table('orders')
+    '''
+    ordersTable.update_item(
+        Key={
+            "orderId": orderId
+        },
+        UpdateExpression="set walletId=:wid, walletAdrress=:wad",
+        ExpressionAttributeValues={
+            ":wid": walletId,
+            ":wad": walletAddress
+        },
+        ReturnValues="ALL_NEW"
+    )
+    '''
