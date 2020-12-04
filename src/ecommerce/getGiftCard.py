2ebc8464a0a1aba8083b5523248d4f93ffb3efc7
theodorebrinkofski <tgb29@cornell.edu>
2020-12-03 19:10:32 -0500

    add pathParameter

--- src/ecommerce/getGiftCard.py
+++ src/ecommerce/getGiftCard.py
@@ -1,8 +1,41 @@
 import boto3
-from datetime import datetime
-import uuid
-from decimal import *
+import json
 
 
 def lambda_handler(event, context):
-    print(event)
+
+    try:
+        buyerName = event['pathParameters']['buyerName']
+        dynamodb = boto3.resource('dynamodb')
+        buyer, rates = getConfig(dynamodb, buyerName)
+        giftCard = {
+            "buyer": buyer,
+            "rates": rates
+        }
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(giftCard)
+        }
+
+        return response
+
+    except Exception as e:
+        response = {
+            "statusCode": 302,
+            "headers": {'Location': 'https://www.sudocoins.com/?msg=invalid'},
+            "body": json.dumps({})
+        }
+
+        return response
+
+
+def getConfig(dynamodb, buyerName):
+    configTable = dynamodb.Table('Config')
+    configKey = "HomePage"
+    response = configTable.get_item(Key={'configKey': configKey})
+
+    config = response['Item']
+    buyer = config['configValue']['buyers'][buyerName]
+    rates = config['rates']
+
+    return buyer, rates
