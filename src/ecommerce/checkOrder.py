45266db4b6ad12c995708de5079761add0abca75
theodorebrinkofski <tgb29@cornell.edu>
2020-12-10 15:31:00 -0500

    add coinbaseCallback and remove not needed lambdas

--- src/ecommerce/checkOrder.py
+++
@@ -1,9 +0,0 @@
-import boto3
-from datetime import datetime
-import uuid
-from decimal import *
-
-
-def lambda_handler(event, context):
-    #only needed if we move checkout to sudo app
-    print(event)
