8ffd6885fe099d504c46c2bf32bc33b72bc26264
theodorebrinkofski <tgb29@cornell.edu>
2020-12-11 19:32:20 -0500

    fixes to make confirmEmail work

--- src/confirmEmail.py
+++ src/confirmEmail.py
@@ -12,13 +12,19 @@ def lambda_handler(event, context):
     userName = params['user_name']
     code = params['confirmation_code']
 
-    finalUrl = url + clientId + '&' + userName + '&' + code
+    finalUrl = url + "client_id=" + clientId + '&' + "user_name=" + userName + '&' + "confirmation_code=" + code
+
+    print(finalUrl)
 
     response = requests.get(finalUrl)
-    (print(response.text.encode('utf8')))
+    print(response.text.encode('utf8'))
 
     response = {
         "statusCode": 302,
         "headers": {'Location': 'https://www.sudocoins.com/?msg=welcome'},
         "body": json.dumps({})
     }
+
+    return response
+
+
