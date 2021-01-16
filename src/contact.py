ec5a1e6003947b0039c15248688816176b23e26c
theodorebrinkofski <tgb29@cornell.edu>
2021-01-16 16:28:57 -0500

    added text message alert for contact us

--- src/contact.py
+++ src/contact.py
@@ -6,18 +6,11 @@ from datetime import datetime
 
 def lambda_handler(event, context):
     print("event=%s userId=%", event, context.identity.cognito_identity_id)
-    """Send contact us message to DB
-    Arguments: userId, message
-    Returns: success message
-    """
+
     dynamodb = boto3.resource('dynamodb')
-
     contactTable = dynamodb.Table(os.environ["CONTACT_TABLE"])
-
     msgId = str(uuid.uuid1())
-
     timeNow = datetime.utcnow().isoformat()
-
     jsonInput = event
 
     message = {
@@ -34,7 +27,13 @@ def lambda_handler(event, context):
         Item=message
     )
 
+    client = boto3.client("sns")
+    client.publish(
+        PhoneNumber="+16282265769",
+        Message="Contact us message submitted"
+    )
+
     return {
         'statusCode': 200,
-        'body': "success"
+        'body': "success1"
     }
