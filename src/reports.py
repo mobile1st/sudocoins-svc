d4ccd98e91501b70d2b141a5eed070f2d1c172fe
theodorebrinkofski <tgb29@cornell.edu>
2021-01-22 02:00:21 -0500

    reports returns sorted values for charts.

--- src/reports.py
+++ src/reports.py
@@ -1,4 +1,5 @@
 import boto3
+from operator import itemgetter
 
 
 def lambda_handler(event, context):
@@ -6,7 +7,28 @@ def lambda_handler(event, context):
     reportsTable = dynamodb.Table('Reports')
 
     reports = reportsTable.scan()
+    reports = reports['Items']
+
+    starts = []
+    completes = []
+    profiles = []
+    revenue = []
+
+    for i in reports:
+        date = i['date']
+        starts.append([date, i['Starts']])
+        completes.append([date, i['Completes']])
+        revenue.append([date, i['Revenue']])
+        profiles.append([date, i['Profiles']])
+
+    starts.sort(key=itemgetter(0), reverse=False)
+    completes.sort(key=itemgetter(0), reverse=False)
+    revenue.sort(key=itemgetter(0), reverse=False)
+    profiles.sort(key=itemgetter(0), reverse=False)
 
     return {
-        "body": reports
+        "starts": starts,
+        "completes": completes,
+        "profiles": profiles,
+        "revenue": revenue
     }
