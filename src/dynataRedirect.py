7120f02379910dbe5b0b19ef87ad203340f56e41
vercy <megavercy@gmail.com>
2021-03-23 11:16:38 +0900

    Extract terms statuses from dynata redirects

--- src/dynataRedirect.py
+++ src/dynataRedirect.py
@@ -2,6 +2,9 @@ import json
 import os
 import boto3
 
+dynamodb = boto3.resource('dynamodb')
+sqs = boto3.resource('sqs')
+
 
 def lambda_handler(event, context):
     print(event)
@@ -17,28 +20,27 @@ def lambda_handler(event, context):
             referer = ""
 
         if event["queryStringParameters"] is None:
-            queryString = {}
+            query_params = {}
             token = "invalid"
         else:
-            queryString = event["queryStringParameters"]
-            if 'status' in queryString:
-                token = queryString['status']
+            query_params = event["queryStringParameters"]
+            token = query_params.get('status')
 
         print(referer)
 
-        msgValue = {
+        msg_value = {
             "referer": referer,
-            "queryStringParameters": queryString,
-            "missingParams": missingParams(queryString, expectedParams)
+            "queryStringParameters": query_params,
+            "missingParams": missingParams(query_params, expectedParams)
         }
 
         response = createRedirect(redirectUrl, token)
 
-        print(msgValue)
+        print(msg_value)
+
+        enqueue_transaction_end_if_not_complete(query_params)
 
         return response
-
-
 
     except Exception as e:
         print(e)
@@ -83,9 +85,30 @@ def missingParams(params, expectedParams):
     return missingParams
 
 
+def enqueue_transaction_end_if_not_complete(params):
+    if params.get('status') == 'C':
+        print(f"SKIP_COMPLETE_ON_REDIRECT {params}")  # redirects are insecure for completes
+        return
 
+    transaction_id = params.get('sub_id')
+    if transaction_id:  # Dynata redirect has the transactionId in the sub_id parameter
+        transaction_table = dynamodb.Table('Transaction')
+        transaction = transaction_table.get_item(Key={'transactionId': transaction_id})['Item']
+        if transaction.get('surveyCode') == params.get('status'):
+            print(f"UP_TO_DATE {transaction_id}")
+            return
 
+        print(f"NEED_UPDATE {transaction_id}")
 
+        user_id = params.get('userId')
+        user_id = user_id[0: 36] if user_id else None
+        msg = {
+            'buyerName': 'dynata',
+            'hashState': False,
+            'queryStringParameters': params.copy()
+        }
+        msg['queryStringParameters']['endUserId'] = user_id
 
-
+        queue = sqs.get_queue_by_name(QueueName='EndTransaction.fifo')
+        queue.send_message(MessageBody=json.dumps(msg), MessageGroupId='EndTransaction')
 
