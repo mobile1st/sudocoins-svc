2ded7f421f80220743c031c72b35e4e8d7139584
vercy <megavercy@gmail.com>
2021-04-05 11:15:21 +0900

    more detailed communication for cint redirects

--- src/survey_end.py
+++ src/survey_end.py
@@ -17,23 +17,43 @@ def lambda_handler(event, context):
         redirect_url, host_url = get_urls()
         referer = event['headers'].get('Referer', "")
 
-        hash_state = verify_hash(host_url, event["queryStringParameters"])
+        query = event["queryStringParameters"]
+        hash_state = verify_hash(host_url, query)
 
         msg_value = {
             "referer": referer,
-            "queryStringParameters": event["queryStringParameters"],
+            "queryStringParameters": query,
             "hashState": hash_state,
             "buyerName": "cint"
         }
         enqueue(msg_value)
 
-        token = 'valid' if hash_state else 'invalid'
+        token = status_to_token(query.get('status', '').lower(), hash_state)
 
         return create_redirect(redirect_url, token)
 
     except Exception as e:
         log.exception(e)
         return create_redirect('https://www.sudocoins.com/?', 'invalid')
+
+
+def status_to_token(status, hash_state):
+    if status == 'np' or status == 'p':  # no project or screen-out
+        return 'P'
+    elif status == 'bl':  # blocked
+        return 'invalid'
+    elif status == 's':  # security concern
+        return 'invalid'
+    elif status == 'oq':  # over quota
+        return 'F'
+    elif status == 't' or status == 'c':  # tentative or not tentative complete
+        if not hash_state:
+            log.warn('hash mismatch for complete returning invalid')
+            return 'invalid'
+        return 'C'
+
+    log.warn(f'encountered unknown cint status={status}')
+    return 'invalid'
 
 
 def create_redirect(redirect_url, token):
@@ -44,18 +64,18 @@ def create_redirect(redirect_url, token):
     }
 
 
-def enqueue(msgValue):
+def enqueue(msg):
     queue = sqs.get_queue_by_name(QueueName='EndTransaction.fifo')
-    queue.send_message(MessageBody=json.dumps(msgValue), MessageGroupId='EndTransaction')
+    queue.send_message(MessageBody=json.dumps(msg), MessageGroupId='EndTransaction')
 
 
-def verify_hash(hostUrl, params):
-    shaUrl = params.get('h')
-    if not shaUrl:
+def verify_hash(host_url, params):
+    sha_url = params.get('h')
+    if not sha_url:
         return False
 
-    url = (hostUrl + "status={0}&sid={1}&tid={2}").format(params["status"], params["sid"], params["tid"])
-    return check_hash(url, shaUrl)
+    url = (host_url + "status={0}&sid={1}&tid={2}").format(params["status"], params["sid"], params["tid"])
+    return check_hash(url, sha_url)
 
 
 def get_urls():
@@ -74,4 +94,3 @@ def check_hash(url, url_hash):
     ).hexdigest()
 
     return signature == url_hash
-
