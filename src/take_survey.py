0a33b6eb54df194767bfea0cc949697d0c20656e
vercy <megavercy@gmail.com>
2021-04-10 10:40:11 +0900

    improved branching in take_survey - exception handling++

--- src/take_survey.py
+++ src/take_survey.py
@@ -9,6 +9,10 @@ import requests
 log = sudocoins_logger.get()
 sns_client = boto3.client('sns')
 dynamodb = boto3.resource('dynamodb')
+
+buyer_redirect = BuyerRedirect(dynamodb)
+transaction_history = history.History(dynamodb)
+
 quality_score_url_pattern = 'https://ipqualityscore.com/api/json/ip/AnfjI4VR0v2VxiEV5S8c9VdRatbJR4vT/{0}?strictness=1&allow_public_access_points=true'
 invalid_response = {"statusCode": 302, "headers": {'Location': 'https://www.sudocoins.com/?msg=invalid'}, "body": '{}'}
 
@@ -19,33 +23,28 @@ def lambda_handler(event, context):
         log.warn('the request does not contain query parameters')
         return invalid_response
 
-    params = event['queryStringParameters']
-    user_id = get_user_id(params)
-    if not user_id:
-        log.warn(f"userId was not determined from params: {params}")
-        return invalid_response
-
-    ip = event.get('requestContext', {}).get('identity', {}).get('sourceIp')
-    fraud_score, ipqs = get_quality_score(ip)
-    if fraud_score and fraud_score > 85:
-        log.warn(f"userId: {user_id} is considered suspicious")
-        dynamodb.Table('Profile').update_item(
-            Key={"userId": user_id},
-            UpdateExpression="set fraud_score=:fs, active=:ac",
-            ExpressionAttributeValues={
-                ":fs": str(fraud_score),
-                ":ac": False
-            },
-            ReturnValues="ALL_NEW"
-        )
-
-        return invalid_response
-
     try:
-        transaction = history.History(dynamodb)
-        data, profile = transaction.insertTransactionRecord(user_id, params['buyerName'], ip, fraud_score, ipqs)
-        log.debug(f'transaction record inserted data: {data}')
-        timestamp = datetime.utcnow().isoformat()
+        params = event['queryStringParameters']
+        ip = event.get('requestContext', {}).get('identity', {}).get('sourceIp')
+        user_id = get_user_id(params)
+        fraud_score, ipqs = get_quality_score(ip)
+
+        if fraud_score and fraud_score > 85:  # fraud check is optional
+            log.warn(f"userId: {user_id} is considered suspicious")
+            dynamodb.Table('Profile').update_item(
+                Key={"userId": user_id},
+                UpdateExpression="set fraud_score=:fs, active=:ac",
+                ExpressionAttributeValues={
+                    ":fs": str(fraud_score),
+                    ":ac": False
+                },
+                ReturnValues="ALL_NEW"
+            )
+            return invalid_response
+
+        data, profile = transaction_history.insertTransactionRecord(user_id, params['buyerName'], ip, fraud_score, ipqs)
+        log.info(f"created transaction: {json.dumps(data)}")
+
         sns_client.publish(
             TopicArn="arn:aws:sns:us-west-2:977566059069:transaction-event",
             MessageStructure='string',
@@ -60,15 +59,13 @@ def lambda_handler(event, context):
                 'source': 'SURVEY_START',
                 'status': 'Started',
                 'awsRequestId': context.aws_request_id,
-                'timestamp': timestamp,
+                'timestamp': datetime.utcnow().isoformat(),
                 'transactionId': data.get('transactionId'),
                 'buyerName': params.get('buyerName'),
             })
         )
 
-        log.info(f'buyer: {params["buyerName"]}')
         entry_url = generate_entry_url(user_id, params['buyerName'], data["transactionId"], ip, profile)
-        log.debug(f'entryUrl generated: {entry_url}')
         return {"statusCode": 302, "headers": {'Location': entry_url}, "body": '{}'}
 
     except Exception as e:
@@ -81,13 +78,13 @@ def get_user_id(params):
         log.info(f"userId from query.userId: {params['userId']}")
         return params['userId']
     elif 'sub' in params:
-        subTable = dynamodb.Table('sub')
-        subResponse = subTable.get_item(Key={'sub': params['sub']})
-        user_id = subResponse['Item']['userId']
+        sub_table = dynamodb.Table('sub')
+        sub_results = sub_table.get_item(Key={'sub': params['sub']})
+        user_id = sub_results['Item']['userId']
         log.info(f"userId from query.sub: {params['sub']} -> userId: {user_id}")
         return user_id
-    else:
-        return None
+
+    raise Exception(f'userId was not determined from params: {params}')
 
 
 def get_quality_score(ip):
@@ -100,7 +97,7 @@ def get_quality_score(ip):
         url = quality_score_url_pattern.format(ip)
         x = requests.get(url)
         quality_score_response = json.loads(x.text)
-        log.debug(f'quality_score_response: {quality_score_response}')
+        log.info(f'quality_score_response: {quality_score_response}')
         return quality_score_response["fraud_score"], quality_score_response
     except Exception as e:
         log.exception(e)
@@ -110,16 +107,17 @@ def get_quality_score(ip):
 def get_survey(buyer):
     response = dynamodb.Table('Config').get_item(Key={'configKey': 'TakeSurveyPage'})
     config = response['Item']["configValue"]
-    if buyer in config["buyer"].keys():
-        return config["buyer"][buyer]
-    return None
+    if buyer not in config["buyer"]:
+        raise Exception(f'Could not find configuration for buyer: {buyer}')
+
+    return config["buyer"][buyer]
 
 
-def generate_entry_url(userId, buyerName, transactionId, ip, profile):
-    survey = get_survey(buyerName)
-    if not survey:
-        return None
+def generate_entry_url(user_id, buyer_name, transaction_id, ip, profile):
+    survey = get_survey(buyer_name)
+    entry_url = buyer_redirect.getRedirect(user_id, buyer_name, survey, ip, transaction_id, profile)
+    if not entry_url or entry_url == '':
+        raise Exception(f'could generate entry url for buyer: {buyer_name} survey: {json.dumps(survey)}')
 
-    redirect = BuyerRedirect(dynamodb)
-    return redirect.getRedirect(userId, buyerName, survey, ip, transactionId, profile)
-
+    log.debug(f'generated entryUrl: {entry_url} for buyer: {buyer_name}')
+    return entry_url
