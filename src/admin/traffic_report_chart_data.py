c3ca5b6ff3057e882e7ca1c1b5fc38bef7bb5c7f
vercy <megavercy@gmail.com>
2021-04-13 11:58:43 +0900

    traffic report responsiveness - step 1

--- src/admin/traffic_report_chart_data.py
+++ src/admin/traffic_report_chart_data.py
@@ -22,72 +22,98 @@ def lambda_handler(event, context):
     for item in items:
         reports[item['date']] = item
 
-    mva7_profiles_deque = collections.deque(maxlen=7)
-    mva7_revenue_deque = collections.deque(maxlen=7)
-    mva7_completes_deque = collections.deque(maxlen=7)
-    mva7_profiles = []
-    mva7_revenue = []
-    mva7_completes = []
-    starts = []
-    completes = []
-    terms = []
-    profiles = []
-    revenue = []
-
     all_buyers = set()
     for r in reports.values():
         all_buyers.update(r.get('buyer', {}).keys())
 
-    buyer_filter = {event.get('buyerName')} if event.get('buyerName') else all_buyers
+    total_report = BuyerReport()
+    buyer_reports = {}
+    for buyer_key in all_buyers:
+        buyer_reports[buyer_key] = BuyerReport()
+
+    total_profiles_mva7_deque = collections.deque(maxlen=7)
+    total_profiles_mva7 = []
+    total_profiles_daily = []
+
     for index, key in enumerate(keys):
         date = key['date']
         epoch_date = to_epoch_millis(date)
         item = reports.get(date, default_item.copy())
         buyer = item.get('buyer', {})
 
-        daily_counters = DailyCounters()
-        for buyer_name in buyer_filter.intersection(buyer.keys()):
-            daily_counters.add(buyer[buyer_name])
+        total_report.add(buyer.values(), epoch_date)
+        for buyer_key in all_buyers:
+            buyer_reports[buyer_key].add([buyer.get(buyer_key)], epoch_date)
 
         daily_profiles = int(item.get('profiles', 0))
+        total_profiles_daily.append({'x': epoch_date, 'y': daily_profiles})
+        total_profiles_mva7_deque.append(daily_profiles)
+        if len(total_profiles_mva7_deque) == 7:
+            total_profiles_mva7.append({'x': epoch_date, 'y': round(sum(total_profiles_mva7_deque) / len(total_profiles_mva7_deque))})
 
-        starts.append({'x': epoch_date, 'y': daily_counters.daily_starts})
-        completes.append({'x': epoch_date, 'y': daily_counters.daily_completes})
-        terms.append({'x': epoch_date, 'y': daily_counters.daily_terms})
-        profiles.append({'x': epoch_date, 'y': daily_profiles})
-        revenue.append({'x': epoch_date, 'y': daily_counters.daily_revenue})
-
-        mva7_profiles_deque.append(daily_profiles)
-        mva7_revenue_deque.append(daily_counters.daily_revenue)
-        mva7_completes_deque.append(daily_counters.daily_completes)
-
-        if index >= 6:
-            mva7_profiles.append({'x': epoch_date, 'y': round(sum(mva7_profiles_deque) / len(mva7_profiles_deque))})
-            mva7_revenue.append({'x': epoch_date, 'y': sum(mva7_revenue_deque) / len(mva7_revenue_deque)})
-            mva7_completes.append({'x': epoch_date, 'y': sum(mva7_completes_deque) / len(mva7_completes_deque)})
-
-    return {
+    result = {
         'buyers': sorted(all_buyers),
         'lastMva7': {
-            'completes': mva7_completes[len(mva7_completes) - 2]['y'],
-            'profiles': mva7_profiles[len(mva7_profiles) - 2]['y'],
-            'revenue': mva7_revenue[len(mva7_revenue) - 2]['y']
+            'completes': total_report.mva7_completes[len(total_report.mva7_completes) - 2]['y'],
+            'profiles': total_profiles_mva7[len(total_profiles_mva7) - 2]['y'],
+            'revenue': total_report.mva7_revenue[len(total_report.mva7_revenue) - 2]['y']
         },
         'trafficChart': {
-            'starts': starts,
-            'completes': completes,
-            'terms': terms,
-            'mva7Completes': mva7_completes
+            'starts': total_report.starts,
+            'completes': total_report.completes,
+            'terms': total_report.terms,
+            'mva7Completes': total_report.mva7_completes
         },
         'profileChart': {
-            'profiles': profiles,
-            'mva7': mva7_profiles
+            'profiles': total_profiles_daily,
+            'mva7': total_profiles_mva7
         },
         'revenueChart': {
-            'revenue': revenue,
-            'mva7': mva7_revenue
-        }
+            'revenue': total_report.revenue,
+            'mva7': total_report.mva7_revenue
+        },
+        'total': {
+            'totals': {
+                'completes': total_report.mva7_completes[len(total_report.mva7_completes) - 2]['y'],
+                'profiles': total_profiles_mva7[len(total_profiles_mva7) - 2]['y'],
+                'revenue': total_report.mva7_revenue[len(total_report.mva7_revenue) - 2]['y']
+            },
+            'traffic': {
+                'starts': total_report.starts,
+                'completes': total_report.completes,
+                'terms': total_report.terms
+            },
+            'revenue': {
+                'daily': total_report.revenue,
+                'mva7': total_report.mva7_revenue
+            },
+            'profiles': {
+                'daily': total_profiles_daily,
+                'mva7': total_profiles_mva7
+            }
+        },
     }
+
+    for buyer, report in buyer_reports.items():
+        result |= {
+            buyer: {
+                'totals': {
+                    'completes': report.mva7_completes[len(report.mva7_completes) - 2]['y'],
+                    'revenue': report.mva7_revenue[len(report.mva7_revenue) - 2]['y']
+                },
+                'traffic': {
+                    'starts': report.starts,
+                    'completes': report.completes,
+                    'terms': report.terms
+                },
+                'revenue': {
+                    'daily': report.revenue,
+                    'mva7': report.mva7_revenue
+                }
+            }
+        }
+
+    return result
 
 
 def generate_input_keys():  # this is controlling the flow, guarantees sorting
@@ -104,10 +130,11 @@ def to_epoch_millis(date_string):
 
 
 class DailyCounters:
-    daily_completes = 0
-    daily_terms = 0
-    daily_starts = 0
-    daily_revenue = 0
+    def __init__(self):
+        self.daily_completes = 0
+        self.daily_terms = 0
+        self.daily_starts = 0
+        self.daily_revenue = 0
 
     def add(self, buyer_counters):
         if buyer_counters is None:
@@ -118,3 +145,32 @@ class DailyCounters:
         self.daily_terms += buyer_daily_terms
         self.daily_starts += max(int(buyer_counters.get('starts', 0)) - buyer_daily_terms - buyer_daily_completes, 0)
         self.daily_revenue += float(buyer_counters.get('revenue', 0) / 100)
+
+
+class BuyerReport:
+    def __init__(self):
+        self.mva7_revenue_deque = collections.deque(maxlen=7)
+        self.mva7_completes_deque = collections.deque(maxlen=7)
+        self.mva7_revenue = []
+        self.mva7_completes = []
+        self.starts = []
+        self.completes = []
+        self.terms = []
+        self.revenue = []
+
+    def add(self, buyers, epoch_date):
+        daily_counters = DailyCounters()
+        for buyer in buyers:
+            daily_counters.add(buyer)
+
+        self.starts.append({'x': epoch_date, 'y': daily_counters.daily_starts})
+        self.completes.append({'x': epoch_date, 'y': daily_counters.daily_completes})
+        self.terms.append({'x': epoch_date, 'y': daily_counters.daily_terms})
+        self.revenue.append({'x': epoch_date, 'y': daily_counters.daily_revenue})
+
+        self.mva7_revenue_deque.append(daily_counters.daily_revenue)
+        self.mva7_completes_deque.append(daily_counters.daily_completes)
+
+        if len(self.mva7_revenue_deque) == 7:
+            self.mva7_revenue.append({'x': epoch_date, 'y': sum(self.mva7_revenue_deque) / len(self.mva7_revenue_deque)})
+            self.mva7_completes.append({'x': epoch_date, 'y': sum(self.mva7_completes_deque) / len(self.mva7_completes_deque)})
