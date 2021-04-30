d6c7b5f8fe2c73a612ea3bdbc71feab70666eb9c
theodorebrinkofski <tgb29@cornell.edu>
2021-04-30 00:03:31 -0400

    myGallery file for get_uploads by userId

---
+++ src/myGallery.py
@@ -0,0 +1,20 @@
+import boto3
+from art import Art
+import sudocoins_logger
+from datetime import datetime
+
+log = sudocoins_logger.get()
+dynamodb = boto3.resource('dynamodb')
+art = Art(dynamodb)
+
+
+def lambda_handler(event, context):
+    # returns the art shared by the user
+
+    my_art = art.get_recent(event['userId'])
+
+
+    return {
+        'statusCode': 200,
+        'art': my_art['Items']
+    }
