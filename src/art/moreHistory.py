a88c1d33318bbce49509085cc8c227e007216850
theodorebrinkofski <tgb29@cornell.edu>
2021-06-28 19:41:54 -0400

    add json loads to more history

--- src/art/moreHistory.py
+++ src/art/moreHistory.py
@@ -1,11 +1,13 @@
 import boto3
 from art.history import History
+import json
 
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
+    request_data = json.loads(event['body'])
     subTable = dynamodb.Table('sub')
-    sub = event['sub']
+    sub = request_data['sub']
     subResponse = subTable.get_item(Key={'sub': sub})
 
     if 'Item' in subResponse:
