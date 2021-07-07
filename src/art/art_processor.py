b677c332e7af96eaa69f5649724b0e01ff39054a
vercy <megavercy@gmail.com>
2021-07-07 10:45:25 +0900

    art_processor switching to http.client

--- src/art/art_processor.py
+++ src/art/art_processor.py
@@ -2,7 +2,9 @@ import boto3
 import json
 from util import sudocoins_logger
 from art.art import Art
-import requests
+import mimetypes
+import http.client
+from urllib.parse import urlparse
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
@@ -27,28 +29,20 @@ def stream_to_s3(data):
         },
         ReturnValues="UPDATED_NEW"
     )
-    art_url = data['art_url']
-    response = requests.get(art_url, stream=True)
-
-    file_type = response.headers['content-type']
-    file_size = response.headers['content-length']
-    type_index = file_type.find('/')
-    file_ending = file_type[type_index + 1:]
-    log.info(response.headers)
+    file = download(data['art_url'])
 
     s3_bucket = "art-processor-bucket"
-    s3_file_path = data['art_id'] + '.' + file_ending
-    response.raw.decode_content = True
+    s3_file_path = data['art_id'] + file['extension']
     client = boto3.client('s3')
-    client.put_object(Bucket=s3_bucket, Body=response.content, Key=s3_file_path, ContentType=file_type)
+    client.put_object(Bucket=s3_bucket, Body=file['bytes'], Key=s3_file_path, ContentType=file['mimeType'])
     log.info('upload to s3 finished')
 
     art_table.update_item(
         Key={'art_id': data['art_id']},
         UpdateExpression="SET file_type=:ft, size=:size, process_status=:ps",
         ExpressionAttributeValues={
-            ':ft': file_type,
-            ':size': file_size,
+            ':ft': file['mimeType'],
+            ':size': len(file['bytes']),
             ':ps': "processed"
         },
         ReturnValues="UPDATED_NEW"
@@ -56,3 +50,17 @@ def stream_to_s3(data):
     log.info("art file type and size added to art table")
 
     return
+
+
+def download(url: str):
+    log.info(f'download {url}')
+    url = urlparse(url)
+    conn = http.client.HTTPSConnection(url.hostname)
+    conn.request("GET", url.path)
+    response = conn.getresponse()
+    length = response.getheader('Content-Length')
+    content_type = response.getheader('Content-Type')
+    log.info(f'download status: {response.status} starting to read: {content_type} {length if length else "?"} bytes')
+    content_bytes = response.read()
+    log.info(f'download success')
+    return {'mimeType': content_type, 'bytes': content_bytes, 'extension': mimetypes.guess_extension(content_type)}
