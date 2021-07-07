7bd07f92550a72d429f3024b8df723dd1d60897e
theodorebrinkofski <tgb29@cornell.edu>
2021-07-06 23:11:02 -0400

    script changes

--- src/art/scan_and_process.py
+++ src/art/scan_and_process.py
@@ -1,59 +1,68 @@
 import boto3
-from botocore.exceptions import ClientError
-from boto3.dynamodb.conditions import Key, Attr
-import botocore
-import requests
+import json
+import mimetypes
+import http.client
+from urllib.parse import urlparse
+import ssl
+
+
+dynamodb = boto3.resource('dynamodb')
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def stream_to_s3(data):
-    dynamodb = boto3.resource('dynamodb')
-    art_url = data['art_url']['S']
-    response = requests.get(art_url, stream=True)
-
-    if 'content-type' in response.headers:
-        file_type = response.headers['content-type']
-    else:
-        file_type = None
-
-    if 'content-length' in response.headers:
-        file_size = response.headers['content-length']
-    else:
-        file_type = None
-
-    type_index = file_type.find('/')
-    file_ending = file_type[type_index + 1:]
-    # print(response.headers)
-
-    s3 = boto3.client('s3')
-    s3_bucket = "art-processor-bucket"
-    s3_file_path = data['art_id']['S'] + '.' + file_ending
-    response.raw.decode_content = True
-    conf = boto3.s3.transfer.TransferConfig(multipart_threshold=10000, max_concurrency=4)
-    s3.upload_fileobj(response.raw, s3_bucket, s3_file_path, Config=conf)
-    print('upload to s3 finished')
-
     art_table = dynamodb.Table('art')
     art_table.update_item(
         Key={'art_id': data['art_id']['S']},
-        UpdateExpression="SET file_type=:ft, size=:size",
+        UpdateExpression="SET process_status=:ps",
         ExpressionAttributeValues={
-            ':ft': file_type,
-            ':size': file_size
+            ':ps': "attempted"
         },
         ReturnValues="UPDATED_NEW"
     )
-    print("art file type and size added to art table")
+    file = download(data['art_url']['S'])
+
+    s3_bucket = "art-processor-bucket"
+    s3_file_path = data['art_id']['S'] + mimetypes.guess_extension(file['mimeType'])
+    client = boto3.client('s3')
+    client.put_object(Bucket=s3_bucket, Body=file['bytes'], Key=s3_file_path, ContentType=file['mimeType'])
+
+    art_table.update_item(
+        Key={'art_id': data['art_id']['S']},
+        UpdateExpression="SET file_type=:ft, size=:size, process_status=:ps",
+        ExpressionAttributeValues={
+            ':ft': file['mimeType'],
+            ':size': len(file['bytes']),
+            ':ps': "processed"
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    print("file uploaded")
 
     return
 
 
+def download(url: str):
+    url = urlparse(url)
+    conn = http.client.HTTPSConnection(url.hostname, timeout=15)
+    conn.request("GET", url.path)
+    response = conn.getresponse()
+    length = response.getheader('Content-Length')
+    content_type = response.getheader('Content-Type')
+    content_bytes = response.read()
+    return {'mimeType': content_type, 'bytes': content_bytes}
+
+
+
 client2 = boto3.client('dynamodb')
 response2 = client2.scan(
-    TableName='art')
+TableName='art')
 data = response2['Items']
 while 'LastEvaluatedKey' in response2:
-    response2 = client2.scan(TableName='art', ExclusiveStartKey=response2['LastEvaluatedKey'])
+    response2 = client2.scan(TableName='art',ExclusiveStartKey=response2['LastEvaluatedKey'])
     data.extend(response2['Items'])
+
 
 client = boto3.client('s3')
 response = client.list_objects(
@@ -63,22 +72,23 @@ keys = []
 for i in response['Contents']:
     index = i['Key'].find('.')
     keys.append(i['Key'][:index])
-# print(keys)
+
 
 bad_ids = []
 count = 0
+count2 = 0
 for i in data:
     try:
         if i['art_id']['S'] in keys:
-            print("already added")
+            count2 += 1
+            print("added count: " + str(count2))
         else:
-            print(i)
             stream_to_s3(i)
-
             count += 1
-            print(count)
-            print(i['art_id']['S'])
-    except:
+            print("uploaded count: " + str(count))
+            #print(i['art_id']['S'])
+    except Exception as e:
+        print(e)
         bad_ids.append(i['art_id']['S'])
         continue
 
