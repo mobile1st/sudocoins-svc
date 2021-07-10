4c3b0c16a4754cdd5273290ff070a81dffc044d9
vercy <megavercy@gmail.com>
2021-07-10 09:19:47 +0900

    write cdn_url after s3 upload

--- src/art/art_processor.py
+++ src/art/art_processor.py
@@ -1,12 +1,14 @@
 import boto3
 import json
 from util import sudocoins_logger
+import mimetypes
 import http.client
 from urllib.parse import urlparse
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
 s3_client = boto3.client('s3')
+cdn_prefix = 'https://cdn.sudocoins.com/'
 
 
 def lambda_handler(event, context):
@@ -23,24 +25,30 @@ def stream_to_s3(data):
     file = download(data['art_url'])
 
     s3_bucket = 'sudocoins-art-bucket'
-    s3_file_path = data['art_id']
+    s3_file_path = data['art_id'] + extension(file['mimeType'])
     s3_client.put_object(Bucket=s3_bucket, Body=file['bytes'], Key=s3_file_path, ContentType=file['mimeType'])
     log.info('upload to s3 finished')
 
     art_table = dynamodb.Table('art')
     art_table.update_item(
         Key={'art_id': data['art_id']},
-        UpdateExpression="SET file_type=:ft, size=:size",
+        UpdateExpression="SET file_type=:ft, size=:size, cdn_url=:cdn_url",
         ExpressionAttributeValues={
             ':ft': file['mimeType'],
-            ':size': len(file['bytes'])  # why do we need this?
-            # TODO add cdn_url: '{prefix}+{art_id}'
+            ':size': len(file['bytes']),  # TODO remove, irrelevant after URL rewrite
+            ':cdn_url': f'{cdn_prefix}{s3_file_path}'
         })
     art_table.update_item(
         Key={'art_id': data['art_id']},
         UpdateExpression="remove process_status")
 
     log.info("art table updated")
+
+
+def extension(mime_type: str):
+    key = mime_type.split(';')[0]
+    ext = mimetypes.guess_extension(key)
+    return ext if ext else ''
 
 
 def download(url: str):
