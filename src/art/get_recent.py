16cca2b5c593f9f72308f3a43b8da14c9a574b76
vercy <megavercy@gmail.com>
2021-07-13 15:22:41 +0900

    use cdn url when present ++

--- src/art/get_recent.py
+++ src/art/get_recent.py
@@ -1,32 +1,21 @@
 import boto3
 from util import sudocoins_logger
 from boto3.dynamodb.conditions import Key
+from art.art import Art
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
-
+arts = Art(dynamodb)
 
 def lambda_handler(event, context):
     set_log_context(event)
     query_params = event['queryStringParameters']
     count = int(query_params['count'])
     return {
-        'art': get_recent(count, query_params['timestamp'])
+        'art': arts.get_recent(count, query_params['timestamp'])
     }
 
 
 def set_log_context(event):
     global log
     log = sudocoins_logger.get(sudocoins_logger.get_ctx(event))
-
-
-def get_recent(count, timestamp):
-    # returns recent art records paginated
-    return dynamodb.Table('art').query(
-        KeyConditionExpression=Key("sort_idx").eq('true') & Key("recent_sk").lt(timestamp),
-        ScanIndexForward=False,
-        Limit=count,
-        IndexName='Recent_index',
-        ProjectionExpression="art_id, preview_url, art_url, #n, click_count, recent_sk, file_type, size",
-        ExpressionAttributeNames={'#n': 'name'}
-    )['Items']
