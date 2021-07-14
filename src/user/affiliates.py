b7966ccdaa9ef736396dcd19f5c0b450815b95c8
theodorebrinkofski <tgb29@cornell.edu>
2021-07-13 19:20:04 -0500

    finished affiliate code. get user, adds sudo, adds ledger

--- src/user/affiliates.py
+++ src/user/affiliates.py
@@ -1,10 +1,12 @@
 import boto3
 import json
 from util import sudocoins_logger
+from art.ledger import Ledger
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
 s3_client = boto3.client('s3')
+ledger = Ledger(dynamodb)
 
 
 def lambda_handler(event, context):
@@ -13,9 +15,21 @@ def lambda_handler(event, context):
 
     shareId = data['shareId']
 
-    # look up shareId and see owner userId (art_uploads)
-    # update affiliate partner's ledger using userId (ledger)
-    # update affiliate partner's earned sudo in profile with userId (profile)
+    user_id = dynamodb.Table('art_uploads').get_item(
+        Key={'shareId': shareId},
+        ProjectionExpression="user_id")['Item']['user_id']
+
+    dynamodb.Table('Profile').update_item(
+        Key={'userId': user_id},
+        UpdateExpression="SET sudocoins = if_not_exists(sudocoins, :start) + :inc",
+        ExpressionAttributeValues={
+            ':inc': 100,
+            ':start': 0
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    ledger.add(100, user_id, 'Affiliate Link Signup')
 
     return shareId
 
