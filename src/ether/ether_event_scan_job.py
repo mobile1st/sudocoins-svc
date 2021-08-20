eef8817554f7a665ed48c313135c48c1f150e04a
wipitech <cowipitech@gmail.com>
2021-08-21 04:59:12 +0800

    update ether scan

--- src/ether/ether_event_scan_job.py
+++ src/ether/ether_event_scan_job.py
@@ -28,7 +28,7 @@ def lambda_handler(event, context):
     loop = asyncio.get_event_loop()
     contract = get_smart_contract(ADDRESS, ABI)
     eventFilter = create_event_filter(contract)
-    group = asyncio.gather(event_loop(eventFilter, 2))
+    group = asyncio.gather(event_loop(eventFilter, 10))
     # timer = Timer(59.0, closeLoop, {group})
     # timer.start()
     try:
