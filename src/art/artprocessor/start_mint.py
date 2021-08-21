d59f73dcdbc58174bf4341331e448b4c19426c27
theodorebrinkofski <tgb29@cornell.edu>
2021-08-21 01:12:38 -0400

    add to art processor

--- src/art/artprocessor/start_mint.py
+++ src/art/artprocessor/start_mint.py
@@ -1,10 +1,10 @@
 import boto3
 from util import sudocoins_logger
-from art.art import Art
+
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
-arts = Art(dynamodb)
+
 
 
 def lambda_handler(event, context):
