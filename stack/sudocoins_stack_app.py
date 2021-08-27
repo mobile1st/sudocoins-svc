0281eb4e5a350722752fe4dc418382da0c8eb21a
Bence Nagykeri <nagykeribence@gmail.com>
2021-08-27 16:34:01 +0200

    explicitly specify environment

--- stack/sudocoins_stack_app.py
+++ stack/sudocoins_stack_app.py
@@ -4,6 +4,10 @@ from aws_cdk import core
 from sudocoins_stack.sudocoins_stack import SudocoinsStack
 
 app = core.App()
-SudocoinsStack(app, "SudocoinsStack")
+SudocoinsStack(
+    app,
+    'SudocoinsStack',
+    env=core.Environment(account='977566059069', region='us-west-2')
+)
 
 app.synth()
