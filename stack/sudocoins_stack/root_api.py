e8898b933a9b41ef03b829b13bca980a754d4045
Bence Nagykeri <nagykeribence@gmail.com>
2021-09-09 19:36:48 +0200

    fix

--- stack/sudocoins_stack/root_api.py
+++ stack/sudocoins_stack/root_api.py
@@ -15,21 +15,20 @@ class SudocoinsRootApi:
                  resources: SudocoinsImportedResources,
                  lambdas: SudocoinsRootLambdas,
                  cors_preflight: typing.Optional[apigwv2.CorsPreflightOptions]):
-        print('asd')
-        # root_api_v1 = apigwv2.HttpApi(
-        #     scope,
-        #     'RootApiV1',
-        #     default_domain_mapping=apigwv2.DomainMappingOptions(
-        #         domain_name=resources.sudocoins_domain_name
-        #     ),
-        #     cors_preflight=cors_preflight
-        # )
-        # # SOCIAL SHARE
-        # social_share_integration = api_integrations.LambdaProxyIntegration(
-        #     handler=lambdas.social_share_function
-        # )
-        # root_api_v1.add_routes(
-        #     path='/a/{shareId}',
-        #     methods=[apigwv2.HttpMethod.GET],
-        #     integration=social_share_integration
-        # )
+        root_api_v1 = apigwv2.HttpApi(
+            scope,
+            'RootApiV1',
+            default_domain_mapping=apigwv2.DomainMappingOptions(
+                domain_name=resources.sudocoins_domain_name
+            ),
+            cors_preflight=cors_preflight
+        )
+        # SOCIAL SHARE
+        social_share_integration = api_integrations.LambdaProxyIntegration(
+            handler=lambdas.social_share_function
+        )
+        root_api_v1.add_routes(
+            path='/a/{shareId}',
+            methods=[apigwv2.HttpMethod.GET],
+            integration=social_share_integration
+        )
