220bbb90e3ebceea02e6f9288b03df61ee334efd
Bence Nagykeri <nagykeribence@gmail.com>
2021-09-15 23:42:49 +0200

    add pyvips example

--- src/search/test_dependency.py
+++ src/search/test_dependency.py
@@ -1,5 +1,8 @@
 import requests
+import pyvips
 
 
 def lambda_handler(event, context):
     print(requests.get('https://google.com'))
+    image = pyvips.Image.new_from_file('test.svg')
+    image.write_to_file('test.png')
