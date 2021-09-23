3b7569b20b0f681b708f4900bde87291b266dc04
theodorebrinkofski <tgb29@cornell.edu>
2021-09-23 17:39:27 -0400

    change set trending to add more time views to collections

--- src/art/set_trending.py
+++ src/art/set_trending.py
@@ -8,14 +8,14 @@ dynamodb = boto3.resource('dynamodb')
 
 
 def lambda_handler(event, context):
-    hour, half_day, day, leaders = get_trending()
+    hour, half_day, day, leaders, leaders_hour, leaders_half = get_trending()
 
-    set_config(hour, half_day, day, leaders)
+    set_config(hour, half_day, day, leaders, leaders_hour, leaders_half)
 
     return
 
 
-def set_config(hour, half_day, day, artists):
+def set_config(hour, half_day, day, artists, artists_hour, artists_half):
     config_table = dynamodb.Table('Config')
 
     if len(hour) == 0:
@@ -38,9 +38,11 @@ def set_config(hour, half_day, day, artists):
     )
     config_table.update_item(
         Key={'configKey': 'Leaderboard'},
-        UpdateExpression="set creators=:create",
+        UpdateExpression="set creators=:create, creators_hour=:create2, creators_half=:create2",
         ExpressionAttributeValues={
-            ":create": artists
+            ":create": artists,
+            ":create2": artists_hour,
+            ":create3": artists_half
         }
     )
     log.info("configs updated")
@@ -80,35 +82,24 @@ def get_trending():
     hour = []
     half_day = []
     day = []
+
     artists = {}
+    artists_hour = {}
+    artists_half = {}
 
     for i in sorted_arts:
         try:
             if i['blockchain'] != 'Ethereum':
                 continue
             day.append(i['art_id'])
+            artists = add_collection(i, artists)
+
             if i['event_date'] > (datetime.fromisoformat(created) - timedelta(hours=1)).isoformat():
                 hour.append(i['art_id'])
+                artists_hour = add_collection(i, artists_hour)
             if i['event_date'] > (datetime.fromisoformat(created) - timedelta(hours=12)).isoformat():
                 half_day.append(i['art_id'])
-
-            if i['collection_data']['name'] in artists:
-                artists[i['collection_data']['name']]['score'] += i.get('last_sale_price')
-            else:
-                artists[i['collection_data']['name']] = {}
-                artists[i['collection_data']['name']]['score'] = i.get('last_sale_price')
-                artists[i['collection_data']['name']]['avatar'] = i.get('preview_url')
-
-                artists[i['collection_data']['name']]['data'] = {}
-                artists[i['collection_data']['name']]['data']['address'] = i.get('collection_address')
-                artists[i['collection_data']['name']]['data']['profile_img_url'] = i.get('preview_url')
-                artists[i['collection_data']['name']]['data']['user'] = i.get('open_sea_data', {}).get('creator')
-
-                artists[i['collection_data']['name']]['art1'] = i.get('art_id')
-                artists[i['collection_data']['name']]['name'] = i.get('collection_data', {}).get('name', i['creator'])
-                artists[i['collection_data']['name']]['collection_address'] = i.get('collection_address')
-                if artists[i['collection_data']['name']]['collection_address'] == 'unknown':
-                    artists[i['collection_data']['name']]['collection_address'] = i['open_sea_data']['asset']['asset_contract']['address']
+                artists_half = add_collection(i, artists_half)
 
 
         except Exception as e:
@@ -117,7 +108,33 @@ def get_trending():
             continue
 
     leaders = sorted(artists.values(), key=lambda x: x['score'], reverse=True)[:250]
+    leaders_hour = sorted(artists_hour.values(), key=lambda x: x['score'], reverse=True)[:250]
+    leaders_half = sorted(artists_half.values(), key=lambda x: x['score'], reverse=True)[:250]
 
-    return hour[0:250], half_day[0:250], day[0:250], leaders
+    return hour[0:250], half_day[0:250], day[0:250], leaders, leaders_hour, leaders_half
+
+
+def add_collection(i, artists):
+    if i['collection_data']['name'] in artists:
+        artists[i['collection_data']['name']]['score'] += i.get('last_sale_price')
+    else:
+        artists[i['collection_data']['name']] = {}
+        artists[i['collection_data']['name']]['score'] = i.get('last_sale_price')
+        artists[i['collection_data']['name']]['avatar'] = i.get('preview_url')
+
+        artists[i['collection_data']['name']]['data'] = {}
+        artists[i['collection_data']['name']]['data']['address'] = i.get('collection_address')
+        artists[i['collection_data']['name']]['data']['profile_img_url'] = i.get('preview_url')
+        artists[i['collection_data']['name']]['data']['user'] = i.get('open_sea_data', {}).get('creator')
+
+        artists[i['collection_data']['name']]['art1'] = i.get('art_id')
+        artists[i['collection_data']['name']]['name'] = i.get('collection_data', {}).get('name', i['creator'])
+        artists[i['collection_data']['name']]['collection_address'] = i.get('collection_address')
+        if artists[i['collection_data']['name']]['collection_address'] == 'unknown':
+            artists[i['collection_data']['name']]['collection_address'] = i['open_sea_data']['asset']['asset_contract'][
+                'address']
+
+    return artists
+
 
 
