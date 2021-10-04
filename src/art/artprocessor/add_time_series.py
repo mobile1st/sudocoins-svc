21b101f9fa98c5e24d67a05cfc8ed543e515754c
theodorebrinkofski <tgb29@cornell.edu>
2021-10-04 16:13:22 -0400

    first draft of time series edits. init time_series table

--- src/art/artprocessor/add_time_series.py
+++ src/art/artprocessor/add_time_series.py
@@ -1,6 +1,7 @@
 import boto3
 from util import sudocoins_logger
 import json
+import statistics
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
@@ -11,6 +12,38 @@ def lambda_handler(event, context):
     art = json.loads(event['Records'][0]['Sns']['Message'])
     # art = event['Records'][0]['Sns']['Message']
 
-    log.info(f'received message')
+    timestamp = art['event_date'].split('T')[0]
+    collection_id = art['collection_id']
+    lsp = art['last_sale_price']
+
+    record = dynamodb.Table('time_series').get_item(Key={'date': timestamp, 'collection_id': collection_id})
+    if 'Item' in record:
+        trades = record['Item']['trades']
+        trades.append(lsp)
+        floor = min(trades)
+        median = statistics.median(trades)
+
+        dynamodb.Table('time_series').update_item(
+            Key={'date': timestamp, 'collection_id': collection_id},
+            UpdateExpression="set trades=:tr, median=:me, floor=:fl ",
+            ExpressionAttributeValues={
+                ":tr": trades,
+                ":me": median,
+                "fl": floor
+            },
+            ReturnValues="ALL_NEW"
+        )
+        log.info(f'record updated')
+    else:
+        new_record = {
+            "date": timestamp,
+            "collection_id": collection_id,
+            "median": lsp,
+            "trades": [lsp],
+            "floor": lsp
+        }
+        dynamodb.Table('time_series').put_item(Item=new_record)
+        log.info(f'record created')
+
 
     return
