455a5a466870acac838b33206bba553f33784d38
theodorebrinkofski <tgb29@cornell.edu>
2021-10-05 21:20:36 -0400

    added buyers to the config

--- src/art/set_trending.py
+++ src/art/set_trending.py
@@ -76,14 +76,16 @@ def get_trending():
     record = dynamodb.Table('art').query(
         KeyConditionExpression=Key("sort_idx").eq('true') & Key("event_date").gt(period),
         IndexName='top-sales',
-        ProjectionExpression="art_id, last_sale_price, event_date, creator, preview_url, open_sea_data, collection_data, collection_address, blockchain, owner"
+        ProjectionExpression="art_id, last_sale_price, event_date, creator, preview_url, open_sea_data, collection_data, collection_address, blockchain, #o",
+        ExpressionAttributeNames={'#o': 'owner'}
     )
     data = record['Items']
     while 'LastEvaluatedKey' in record:
         record = dynamodb.Table('art').query(
             KeyConditionExpression=Key("sort_idx").eq('true') & Key("event_date").gt(period),
             IndexName='top-sales',
-            ProjectionExpression="art_id, last_sale_price, event_date, creator, preview_url, open_sea_data, collection_data, collection_address, blockchain, owner",
+            ProjectionExpression="art_id, last_sale_price, event_date, creator, preview_url, open_sea_data, collection_data, collection_address, blockchain, #o",
+            ExpressionAttributeNames={'#o': 'owner'},
             ExclusiveStartKey=record['LastEvaluatedKey']
         )
         data.extend(record['Items'])
@@ -111,11 +113,11 @@ def get_trending():
                 hour.append(i['art_id'])
 
                 if i['owner'] in owners2:
-                    owners2[i['owner']['score']] += i.get('last_sale_price')
+                    owners2[i['owner']]['score'] += i.get('last_sale_price')
                 else:
                     owners2[i['owner']] = {}
-                    owners2[i['owner']['score']] = i.get('last_sale_price')
-                    owners2[i['owner']['preview_url']] = i.get('preview_url')
+                    owners2[i['owner']]['score'] = i.get('last_sale_price')
+                    owners2[i['owner']]['preview_url'] = i.get('preview_url')
 
                 if i['collection_data']['name'] in artists2:
                     artists2[i['collection_data']['name']]['score'] += i.get('last_sale_price')
@@ -141,11 +143,11 @@ def get_trending():
                 half_day.append(i['art_id'])
 
                 if i['owner'] in owners3:
-                    owners3[i['owner']['score']] += i.get('last_sale_price')
+                    owners3[i['owner']]['score'] += i.get('last_sale_price')
                 else:
                     owners3[i['owner']] = {}
-                    owners3[i['owner']['score']] = i.get('last_sale_price')
-                    owners3[i['owner']['preview_url']] = i.get('preview_url')
+                    owners3[i['owner']]['score'] = i.get('last_sale_price')
+                    owners3[i['owner']]['preview_url'] = i.get('preview_url')
 
                 if i['collection_data']['name'] in artists3:
                     artists3[i['collection_data']['name']]['score'] += i.get('last_sale_price')
@@ -170,11 +172,11 @@ def get_trending():
             day.append(i['art_id'])
 
             if i['owner'] in owners:
-                owners[i['owner']['score']] += i.get('last_sale_price')
+                    owners[i['owner']]['score'] += i.get('last_sale_price')
             else:
                 owners[i['owner']] = {}
-                owners[i['owner']['score']] = i.get('last_sale_price')
-                owners[i['owner']['preview_url']] = i.get('preview_url')
+                owners[i['owner']]['score'] = i.get('last_sale_price')
+                owners[i['owner']]['preview_url'] = i.get('preview_url')
 
             if i['collection_data']['name'] in artists:
                 artists[i['collection_data']['name']]['score'] += i.get('last_sale_price')
@@ -206,8 +208,8 @@ def get_trending():
     leaders_hour = sorted(artists2.values(), key=lambda x: x['score'], reverse=True)
     leaders_half = sorted(artists3.values(), key=lambda x: x['score'], reverse=True)
 
-    buyers = sorted(owners.values(), key=lambda x: x['score'], reverse=True)[:200]
-    buyers_hour = sorted(owners2.values(), key=lambda x: x['score'], reverse=True)[:200]
-    buyers_half = sorted(owners3.values(), key=lambda x: x['score'], reverse=True)[:200]
+    buyers = sorted(owners.values(), key=lambda x: x['score'], reverse=True)
+    buyers_hour = sorted(owners2.values(), key=lambda x: x['score'], reverse=True)
+    buyers_half = sorted(owners3.values(), key=lambda x: x['score'], reverse=True)
 
-    return hour[0:250], half_day[0:250], day[0:250], leaders[0:200], leaders_hour[0:200], leaders_half[0:200], buyers, buyers_half, buyers_hour
+    return hour[0:250], half_day[0:250], day[0:250], leaders[0:175], leaders_hour[0:175], leaders_half[0:175], buyers[0:125], buyers_half[0:125], buyers_hour[0:125]
