b458cec4d0e7b5529a1dd35219b4c04b8227db84
theodorebrinkofski <tgb29@cornell.edu>
2021-10-06 16:23:10 -0400

    changes to time series data

--- src/art/artprocessor/add_time_series.py
+++ src/art/artprocessor/add_time_series.py
@@ -17,40 +17,16 @@ def lambda_handler(event, context):
     collection_id = art['collection_id']
     lsp = art['last_sale_price']
 
-    record = dynamodb.Table('time_series').get_item(Key={'date': timestamp, 'collection_id': collection_id})
-    if 'Item' in record:
-        trades = record['Item']['trades']
-        trades.append(lsp)
+    dynamodb.Table('time_series').update_item(
+        Key={'date': timestamp, 'collection_id': collection_id},
+        UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i)",
+        ExpressionAttributeValues={
+            ':i': [lsp],
+            ':empty_list': []
+        },
+        ReturnValues="UPDATED_NEW"
+    )
 
-        values = {
-            ":tr": trades,
-            ":me": statistics.median(trades),
-            ":fl": min(trades),
-            ":tc": len(trades),
-            ":ce": max(trades)
-        }
-
-        dynamodb.Table('time_series').update_item(
-            Key={'date': timestamp, 'collection_id': collection_id},
-            UpdateExpression="set trades=:tr, median=:me, floor=:fl, trade_count=:tc, ceiling=:ce ",
-            ExpressionAttributeValues=values,
-            ReturnValues="ALL_NEW"
-        )
-        log.info(f'record updated')
-        # . log.info(values)
-
-    else:
-        new_record = {
-            "date": timestamp,
-            "collection_id": collection_id,
-            "median": lsp,
-            "trades": [lsp],
-            "floor": lsp,
-            "trade_count": 1,
-            "celing": lsp
-        }
-        dynamodb.Table('time_series').put_item(Item=new_record)
-        log.info(f'record created')
-        # . log.info(new_record)
+    log.info(f'record updated')
 
     return
