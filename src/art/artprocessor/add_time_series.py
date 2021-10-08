8e34fccbc337e6d20e482816b096c4bb13f7be16
theodorebrinkofski <tgb29@cornell.edu>
2021-10-08 10:58:05 -0400

    update time_series to add map

--- src/art/artprocessor/add_time_series.py
+++ src/art/artprocessor/add_time_series.py
@@ -10,23 +10,54 @@ sns_client = boto3.client("sns")
 
 def lambda_handler(event, context):
     art = json.loads(event['Records'][0]['Sns']['Message'])
-    # art = event['Records'][0]['Sns']['Message']
     log.info(f'art: {art}')
 
     timestamp = art['event_date'].split('T')[0]
     collection_id = art['collection_id']
     lsp = art['last_sale_price']
+    art_id = art['art_id']
 
-    dynamodb.Table('time_series').update_item(
-        Key={'date': timestamp, 'collection_id': collection_id},
-        UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i)",
-        ExpressionAttributeValues={
-            ':i': [lsp],
-            ':empty_list': []
-        },
-        ReturnValues="UPDATED_NEW"
-    )
+    try:
+        response = dynamodb.Table('time_series').update_item(
+            Key={'date': timestamp, 'collection_id': collection_id},
+            UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i),  "
+                             "arts.#art = :k",
+            ExpressionAttributeValues={
+                ':i': [lsp],
+                ':empty_list': [],
+                ':k': lsp
+            },
+            ExpressionAttributeNames={
+                '#art': art_id
+            },
+            ReturnValues="UPDATED_NEW"
+        )
 
-    log.info(f'record updated')
+        log.info(response)
+
+    except Exception as e:
+        if e.response['Error']['Code'] == "ValidationException":
+            dynamodb.Table('time_series').update_item(
+                Key={'date': timestamp, 'collection_id': collection_id},
+                UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i),  "
+                                 "arts = if_not_exists(arts, :new_dict)",
+                ExpressionAttributeValues={
+                    ':i': [lsp],
+                    ':empty_list': [],
+                    ':new_dict': {}
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            dynamodb.Table('time_series').update_item(
+                Key={'date': timestamp, 'collection_id': collection_id},
+                UpdateExpression="SET arts.#art = :i",
+                ExpressionAttributeValues={
+                    ':i': lsp
+                },
+                ExpressionAttributeNames={
+                    '#art': art_id
+                },
+                ReturnValues="UPDATED_NEW"
+            )
 
     return
