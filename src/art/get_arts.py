b18ba3a4c3199cf4d216347462a19a5fa3d7454d
theodorebrinkofski <tgb29@cornell.edu>
2021-10-08 18:19:32 -0400

    get arts removes duplicates

--- src/art/get_arts.py
+++ src/art/get_arts.py
@@ -2,6 +2,7 @@ import boto3
 import json
 from util import sudocoins_logger
 from art.art import Art
+from collections import OrderedDict
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
@@ -11,8 +12,11 @@ art = Art(dynamodb)
 def lambda_handler(event, context):
     set_log_context(event)
     body = json.loads(event['body'])
+    #body = event['arts']
+    arts = list(OrderedDict.fromkeys(body['arts']))
+
     return {
-        'arts': art.get_arts(body['arts'])
+        'arts': art.get_arts(arts)
     }
 
 
