b949fc9091220321b017af1da36cff4704baaf5f
theodorebrinkofski <tgb29@cornell.edu>
2021-10-13 13:54:45 -0400

    change to return uri

--- src/art/minting/set_ipfs.py
+++ src/art/minting/set_ipfs.py
@@ -8,10 +8,28 @@ dynamodb = boto3.resource('dynamodb')
 
 
 def lambda_handler(event, context):
-    # body = json.loads(event['body'])
-    body = event
+    body = json.loads(event['body'])
+    #  body = event
     file_name = body['file_name']
+    name = body['name']
+    description = body['description']
 
+    art_id = file_name.split(".")[0]
+
+    ipfs_image = set_ipfs_image(file_name)
+    ipfs_meta = generate_meta_data(name, description, ipfs_image, art_id)
+
+    return {
+        "uri": ipfs_meta
+    }
+
+
+def set_log_context(event):
+    global log
+    log = sudocoins_logger.get(sudocoins_logger.get_ctx(event))
+
+
+def set_ipfs_image(file_name):
     s3 = boto3.resource('s3')
     tmp = "/tmp/" + file_name
     s3.meta.client.download_file('sudocoins-art-bucket', file_name, tmp)
@@ -30,8 +48,27 @@ def lambda_handler(event, context):
         }
 
 
-def set_log_context(event):
-    global log
-    log = sudocoins_logger.get(sudocoins_logger.get_ctx(event))
+def generate_meta_data(name, description, image, art_id):
+    data = {
+        "name": name,
+        "description": description,
+        "image": image,
+        "external_url": "https://app.sudocoins.com/art/social/" + art_id
+    }
 
+    with open('/tmp/metadata.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    with open('/tmp/metadata.json', 'rb') as meta_json:
+        files = {
+            'file': meta_json
+        }
+        response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files,
+                                 auth=('1xBSq6KuqrbDmhs2ASr722Cs8JF', '3c98ebb9f76fabc45d519718e41dd4f0'))
+
+    ipfs_meta_hash = json.loads(response.text)
+
+    log.info(f'ipfs response {ipfs_meta_hash}')
+
+    return ipfs_meta_hash['Hash']
 
