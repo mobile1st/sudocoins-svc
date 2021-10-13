80f8a579ecc38b9f6ffa6e798103508cbfd1a969
theodorebrinkofski <tgb29@cornell.edu>
2021-10-13 11:30:36 -0400

    switch from floor and median to low and high

--- src/art/set_collection_ts.py
+++ src/art/set_collection_ts.py
@@ -43,46 +43,31 @@ def lambda_handler(event, context):
         response = dynamodb.batch_get_item(RequestItems={'time_series': query})
 
         final_series[collection_id] = {}
-        final_series[collection_id]['floor'] = []
-        final_series[collection_id]['median'] = []
+        final_series[collection_id]['low'] = []
+        final_series[collection_id]['high'] = []
 
         getcontext().prec = 18
 
-        floor1 = 0
-        floor2 = 0
         for row in response['Responses']['time_series']:
-            floor2 = Decimal(min(row['trades'])) / (10 ** 18)
-
-            if floor2 > 0:
-                final_series[collection_id]['floor'].insert(0, {"x": row['date'],
-                                                                "y": Decimal(min(row['trades'])) / (10 ** 18)})
-                floor1 = Decimal(min(row['trades'])) / (10 ** 18)
-            else:
-                if floor1 == 0:
-                    continue
-                else:
-                    final_series[collection_id]['median'].insert(0, {"x": row['date'],
-                                                                     "y": Decimal(statistics.median(row['trades'])) / (
-                                                                             10 ** 18)})
-                    continue
-            final_series[collection_id]['median'].insert(0, {"x": row['date'],
+            final_series[collection_id]['low'].insert(0, {"x": row['date'],
+                                                            "y": Decimal(min(row['trades'])) / (10 ** 18)})
+            final_series[collection_id]['high'].insert(0, {"x": row['date'],
                                                              "y": Decimal(statistics.median(row['trades'])) / (
                                                                      10 ** 18)})
 
+    set_config(final_series)
+
     for k in final_series:
-        floor_list = final_series[k]['floor']
-        median_list = final_series[k]['median']
+        floor_list = final_series[k]['low']
+        median_list = final_series[k]['high']
 
         new_floor_list = sorted(floor_list, key=lambda i: i['x'], reverse=False)
         new_median_list = sorted(median_list, key=lambda i: i['x'], reverse=False)
 
-        final_series[k]['floor'] = new_floor_list
-        final_series[k]['median'] = new_median_list
+        final_series[k]['low'] = new_floor_list
+        final_series[k]['high'] = new_median_list
 
     log.info("config updated")
-    log.info(type(final_series))
-
-    result = set_config(final_series)
 
     return final_series
 
@@ -96,7 +81,7 @@ def get_config():
 def set_config(final_series):
     config_table = dynamodb.Table('Config')
 
-    result = config_table.update_item(
+    config_table.update_item(
         Key={
             'configKey': 'time_series'
         },
@@ -106,5 +91,3 @@ def set_config(final_series):
         },
         ReturnValues="ALL_NEW"
     )
-
-    return result
