f2221b4be9a88e669bae7b291b83b1e75f5fe0b3
theodorebrinkofski <tgb29@cornell.edu>
2021-10-25 10:27:37 -0400

    changes to add time series. adding floor changes each day

--- src/art/artprocessor/add_time_series.py
+++ src/art/artprocessor/add_time_series.py
@@ -2,7 +2,6 @@ import boto3
 from util import sudocoins_logger
 import json
 
-
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
 sns_client = boto3.client("sns")
@@ -13,22 +12,72 @@ def lambda_handler(event, context):
     log.info(f'art: {art}')
 
     timestamp = art['event_date'].split('T')[0]
+
     collection_id = art['collection_id']
     lsp = art['last_sale_price']
     art_id = art['art_id']
+
     if lsp == 0:
         log.info(f'sale price 0: {art_id}')
         return
 
+    floor = update_mappings(collection_id, lsp, art_id)
+    log.info('mappings updated')
 
+    update_trades(timestamp, collection_id, lsp, art_id, floor)
+    log.info('trades updated')
+
+    return
+
+
+def update_trades(timestamp, collection_id, lsp, art_id, floor):
+    try:
+        if floor != 0:
+            response = dynamodb.Table('time_series').update_item(
+                Key={'date': timestamp, 'collection_id': collection_id},
+                UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i),  "
+                                 "trade_count = if_not_exists(trade_count, :st) + :inc,"
+                                 "sales_volume = if_not_exists(trade_count, :st) + :k,"
+                                 "floor = :fl",
+                ExpressionAttributeValues={
+                    ':i': [lsp],
+                    ':empty_list': [],
+                    ':k': lsp,
+                    ':st': 0,
+                    ':inc': 1,
+                    ':fl': floor
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            # . log.info(response)
+        else:
+            response = dynamodb.Table('time_series').update_item(
+                Key={'date': timestamp, 'collection_id': collection_id},
+                UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i),  "
+                                 "trade_count = if_not_exists(trade_count, :st) + :inc,"
+                                 "sales_volume = if_not_exists(trade_count, :st) + :k",
+                ExpressionAttributeValues={
+                    ':i': [lsp],
+                    ':empty_list': [],
+                    ':k': lsp,
+                    ':st': 0,
+                    ':inc': 1
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+
+    except Exception as e:
+        log.info(e)
+
+    return
+
+
+def update_mappings(collection_id, lsp, art_id):
     try:
         response = dynamodb.Table('time_series').update_item(
-            Key={'date': timestamp, 'collection_id': collection_id},
-            UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i),  "
-                             "arts.#art = :k",
+            Key={'date': 'last_sale_price', 'collection_id': collection_id},
+            UpdateExpression="SET arts.#art = :k ",
             ExpressionAttributeValues={
-                ':i': [lsp],
-                ':empty_list': [],
                 ':k': lsp
             },
             ExpressionAttributeNames={
@@ -36,25 +85,27 @@ def lambda_handler(event, context):
             },
             ReturnValues="UPDATED_NEW"
         )
+        # . log.info(response)
+        my_dict = response['Attributes']['arts']
+        floor = min(my_dict.items(), key=lambda x: x[1])[1]
+        log.info(f'floor: {floor}')
 
-        log.info(response)
+        return floor
 
     except Exception as e:
         log.info(e)
         if e.response['Error']['Code'] == "ValidationException":
             dynamodb.Table('time_series').update_item(
-                Key={'date': timestamp, 'collection_id': collection_id},
-                UpdateExpression="SET trades = list_append(if_not_exists(trades, :empty_list), :i),  "
-                                 "arts = if_not_exists(arts, :new_dict)",
+                Key={'date': 'last_sale_price', 'collection_id': collection_id},
+                UpdateExpression="SET arts = if_not_exists(arts, :new_dict)",
                 ExpressionAttributeValues={
-                    ':i': [lsp],
-                    ':empty_list': [],
                     ':new_dict': {}
                 },
                 ReturnValues="UPDATED_NEW"
             )
-            dynamodb.Table('time_series').update_item(
-                Key={'date': timestamp, 'collection_id': collection_id},
+
+            response = dynamodb.Table('time_series').update_item(
+                Key={'date': 'last_sale_price', 'collection_id': collection_id},
                 UpdateExpression="SET arts.#art = :i",
                 ExpressionAttributeValues={
                     ':i': lsp
@@ -64,7 +115,21 @@ def lambda_handler(event, context):
                 },
                 ReturnValues="UPDATED_NEW"
             )
+            # . log.info(response)
+
+            my_dict = response['Attributes']['arts']
+            floor = min(my_dict.items(), key=lambda x: x[1])[1]
+            log.info(f'floor: {floor}')
+
+            return floor
+
         else:
             log.info("other error")
 
-    return
+            floor = 0
+            return floor
+
+
+
+
+
