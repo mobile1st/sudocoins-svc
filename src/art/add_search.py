d71b4e6602d9fb155dfba256b8ff23b5c83597a4
theodorebrinkofski <tgb29@cornell.edu>
2021-10-26 12:54:11 -0400

    improve search keyword processing algo

--- src/art/add_search.py
+++ src/art/add_search.py
@@ -1,10 +1,20 @@
 import boto3
 from util import sudocoins_logger
 import json
+import re
+from collections import Counter
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
 sns_client = boto3.client("sns")
+
+
+def words(text):
+    return re.findall('[a-z]+', text.lower())
+
+
+dictionary = Counter(words(open('big.txt').read()))
+max_word_length = max(map(len, dictionary))
 
 
 def lambda_handler(event, context):
@@ -17,17 +27,25 @@ def lambda_handler(event, context):
     if collection_id is None:
         return
 
-    process_collection(collection_id)
+    collection_name = collection_id.split(":")[1]
+    log.info(f'collection_name: {collection_name}')
+    words = collection_name.split("-")
+    log.info(f'words before processing: {words}')
+
+    for i in words:
+        tmp_list = viterbi_segment(i)[0]
+        words.append(tmp_list)
+
+    tmp_set = set(words)
+    words = list(tmp_set)
+
+    process_collection(collection_id, words)
 
     log.info(f'success')
 
 
-def process_collection(collection_id):
-    collection_name = collection_id.split(":")[1]
-    log.info(f'collection_name: {collection_name}')
-    words = collection_name.split("-")
+def process_collection(collection_id, words):
 
-    log.info(f'words: {words}')
 
     for i in words:
         dynamodb.Table('search').update_item(
@@ -38,5 +56,36 @@ def process_collection(collection_id):
             ExpressionAttributeValues={":i": set([collection_id])},
             ReturnValues="UPDATED_NEW"
         )
+
+
+def viterbi_segment(text):
+    probs, lasts = [1.0], [0]
+    print("1")
+    for i in range(1, len(text) + 1):
+        prob_k, k = max((probs[j] * word_prob(text[j:i]), j)
+                        for j in range(max(0, i - max_word_length), i))
+        probs.append(prob_k)
+        lasts.append(k)
+    words = []
+    print(probs)
+    print(lasts)
+    i = len(text)
+    while 0 < i:
+        words.append(text[lasts[i]:i])
+        i = lasts[i]
+    words.reverse()
+    return words, probs[-1]
+
+
+def word_prob(word):
+    return dictionary[word] / 466560
+
+
+
+
+
+
+
+
 
 
