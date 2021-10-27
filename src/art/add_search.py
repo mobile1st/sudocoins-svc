6c6ff85d22a9eab19a4fb3d198138f7028413b4c
theodorebrinkofski <tgb29@cornell.edu>
2021-10-26 21:10:53 -0400

    fix search to handle more than 100 batch item queries at once

--- src/art/add_search.py
+++ src/art/add_search.py
@@ -1,93 +1,82 @@
 import boto3
 from util import sudocoins_logger
-import json
-import re
-from collections import Counter
+import string
+from boto3.dynamodb.conditions import Key
 
 log = sudocoins_logger.get()
+google_search_host = 'customsearch.googleapis.com'
+search_engine_id = 'abe73c1ca8f9839de'
+api_key = 'AIzaSyA4Be7eS9trAjcz5S4nkxPxKhhpC2IEP6E'
+art_page_prefix = 'https://www.sudocoins.com/art/'
 dynamodb = boto3.resource('dynamodb')
-sns_client = boto3.client("sns")
-
-
-def words(text):
-    return re.findall('[a-z]+', text.lower())
-
-
-dictionary = Counter(words(open('art/big.txt').read()))
-max_word_length = max(map(len, dictionary))
 
 
 def lambda_handler(event, context):
-    art = json.loads(event['Records'][0]['Sns']['Message'])
-    # . art = event['Records'][0]['Sns']['Message']
-    log.info(f'payload: {art}')
+    log.debug(f'event: {event}')
+    query = extract_parameters(event['queryStringParameters'])
+    log.info(query)
 
-    collection_id = art.get('collection_id')
-    if collection_id is None:
-        return
+    key_list = []
+    for i in query:
+        key_list.append({"search_key": i.lower()})
 
-    collection_name = collection_id.split(":")[1]
-    log.info(f'collection_name: {collection_name}')
-    words = collection_name.split("-")
-    log.info(f'words before algo: {words}')
+    query = {
+        'Keys': key_list,
+        'ProjectionExpression': 'collections'
+    }
+    log.info(query)
 
-    new_list = []
-    for i in words:
-        a = viterbi_segment(i)[0]
-        for k in a:
-            new_list.append(k)
+    response = dynamodb.batch_get_item(RequestItems={'search': query})
+    log.info(response)
+    if len(response['Responses']['search']) == 0:
+        return {
+            'arts': []
+        }
 
-    tmp_set = set(words + new_list)
-    words = list(tmp_set)
+    collections = set([])
+    for k in response['Responses']['search']:
+        tmp_set = k['collections']
+        collections.update(tmp_set)
 
-    log.info(f'words before processing: {words}')
+    log.info(collections)
 
-    process_collection(collection_id, words)
-
-
-def process_collection(collection_id, words):
-    for i in words:
-        if i == "":
-            continue
-
-        elif len(i) == 1:
-            continue
-
-        else:
-            dynamodb.Table('search').update_item(
-                Key={
-                    'search_key': i
-                },
-                UpdateExpression="ADD collections :i",
-                ExpressionAttributeValues={":i": set([collection_id])},
-                ReturnValues="UPDATED_NEW"
-            )
+    return {
+        'arts': get_collection_data(collections)
+    }
 
 
-def viterbi_segment(text):
-    probs, lasts = [1.0], [0]
-    for i in range(1, len(text) + 1):
-        prob_k, k = max((probs[j] * word_prob(text[j:i]), j)
-                        for j in range(max(0, i - max_word_length), i))
-        probs.append(prob_k)
-        lasts.append(k)
-    words = []
-    i = len(text)
-    while 0 < i:
-        words.append(text[lasts[i]:i])
-        i = lasts[i]
-    words.reverse()
-    return words, probs[-1]
+def extract_parameters(query_params):
+    query = query_params['q']
+    words = query.split(" ")
+
+    return words
 
 
-def word_prob(word):
-    return dictionary[word] / 466560
+def set_log_context(event):
+    global log
+    log = sudocoins_logger.get(sudocoins_logger.get_ctx(event))
 
 
+def get_collection_data(collections):
+    collection_objects = []
 
+    key_list = []
+    for i in collections:
+        key_list.append({"collection_id": i})
 
+    for k in [key_list[x:x + 100] for x in range(0, len(key_list), 100)]:
+        query = {
+            'Keys': k,
+            'ProjectionExpression': 'collection_id, sale_count, sales_volume, collection_name, preview_url'
+        }
+        log.info(query)
 
+        response = dynamodb.batch_get_item(RequestItems={'collections': query})
 
+        collection_objects = collection_objects + response['Responses']['collections']
 
+    newlist = sorted(collection_objects, key=lambda d: d['sales_volume'], reverse=True)
+
+    return newlist
 
 
