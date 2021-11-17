6dee41a0e8aadfe67ab603d4090c0792926f406b
theodorebrinkofski <tgb29@cornell.edu>
2021-11-16 23:34:29 -0500

    add responses to add-upcoming

--- src/art/add_upcoming.py
+++ src/art/add_upcoming.py
@@ -15,31 +15,43 @@ def lambda_handler(event, context):
     file_name = body['file_name']
     art_id = file_name.split('.')[0]
 
-    form_data = {
-        "upcoming_id": art_id,
-        "timestamp": str(datetime.utcnow().isoformat()),
-        "name": body.get("name"),
-        "description": body.get("description"),
-        "preview_url": f'https://cdn.sudocoins.com/{file_name}',
-        "twitter": body.get("twitter"),
-        "discord": body.get("discord"),
-        "instagram": body.get("instagram"),
-        "website": body.get("website"),
-        "opensea": body.get("opensea"),
-        "total": body.get("total"),
-        "mint_price": body.get("mint_price"),
-        "mint_currency": body.get("mint_currency"),
-        "release_date": body.get("release_date"),
-        "release_time": body.get("release_time"),
-        "presale_time": body.get("presale_time"),
-        "approved": "false"
-    }
+    try:
 
-    dynamodb.Table('upcoming').put_item(
-        Item=form_data
-    )
+        form_data = {
+            "upcoming_id": art_id,
+            "timestamp": str(datetime.utcnow().isoformat()),
+            "name": body.get("name"),
+            "description": body.get("description"),
+            "preview_url": f'https://cdn.sudocoins.com/{file_name}',
+            "twitter": body.get("twitter"),
+            "discord": body.get("discord"),
+            "instagram": body.get("instagram"),
+            "website": body.get("website"),
+            "opensea": body.get("opensea"),
+            "total": body.get("total"),
+            "mint_price": body.get("mint_price"),
+            "mint_currency": body.get("mint_currency"),
+            "release_date": body.get("release_date"),
+            "release_time": body.get("release_time"),
+            "presale_time": body.get("presale_time"),
+            "approved": "false"
+        }
 
-    return
+        dynamodb.Table('upcoming').put_item(
+            Item=form_data
+        )
+
+        return {
+            "status": 200
+        }
+
+    except Exception as e:
+        log.info(e)
+
+        return {
+            "status": 500
+        }
+
 
 
 def set_log_context(event):
