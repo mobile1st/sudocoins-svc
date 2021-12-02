11c7ee04fe37911edc284534f728283bcc4f0208
theodorebrinkofski <tgb29@cornell.edu>
2021-12-02 17:01:03 -0500

    change to auto tweet

--- src/art/marketing/auto_tweet.py
+++ src/art/marketing/auto_tweet.py
@@ -11,10 +11,10 @@ getcontext().prec = 18
 
 
 def lambda_handler(event, context):
-    access_token = "1464273755510042624-OrSgAl8yIo3dNcOUBtCkIHOq68fLQG"
+    access_token = "1464273755510042624-t4pN0z7CPrwq6rbvR8qDfttLruRxwE"
     # SGMbZgiWk0oNjkoet4RKr3DPt
 
-    access_token_secret = "A0a83V9dGjcWaUYlpFzyk6DBGDQJGBwLL5PEAxnJnDlq9"
+    access_token_secret = "2hdTUXyCfjgGp8ObuKm6gMf7iX8cHCBJIsnGsW98iTKDY"
     # fuQEFEzcYxoGzP32CkO7pbvtoMHdKZ8TZYa6W3TGbDiAacHdax
     consumer_key = "FZzJVAxaXk7kXEpev7GNs2AoN"
     consumer_secret = "8zIt0IMRDxHifySEQk8cqWzZVZSnAZaSqZ7StoEugoJcSpzvJ9"
@@ -33,8 +33,8 @@ def lambda_handler(event, context):
     request_uri = 'https://api.twitter.com/1.1/statuses/update.json'
     resp, content = client.request(request_uri, 'POST', urllib.parse.urlencode(data))
 
-    log.info(f"response:  {resp}")
-    log.info(f"content:  {content}")
+    #. log.info(f"response:  {resp}")
+    #. log.info(f"content:  {content}")
 
     return tweet
 
@@ -53,41 +53,40 @@ def get_art():
     art_list = []
 
     for i in trending_art:
-        if count < 1:
-            art = dynamodb.Table('art').get_item(
-                Key={'art_id': i})['Item']
-            try:
-                if 'name' in art['collection_data'] and art['collection_data']['name'] is not None:
-                    if art['collection_data']['name'] in ['Mutant Ape Yacht Club',
-                                                          'Bored Ape Yacht Club, Bored Ape Kennel Club']:
-                        name = art['collection_data']['name']
-                        token_id = art['contractId#tokenId'].split('#')[1]
-                        name_split = name.split()
-                        hashtag = "#" + (name.replace(" ", ""))
-                        message = name_split[0] + " " + name_split[1] + " " + token_id + " sells for "
-                        usd_price = "${:,.2f}".format(
-                            round(((Decimal(art['last_sale_price']) / (10 ** 18)) / eth_rate), 2))
-                        tweet = message + usd_price + " " + art['buy_url'] + " " + hashtag + " #NFT #digitalart"
-                        msg = {
-                            "art_id": i,
-                            "message": tweet,
-                            "timestamp": str(datetime.utcnow().isoformat()),
-                            "platform": "twitter"
-                        }
-                        dynamodb.Table('auto_tweet').put_item(Item=msg)
-                        log.info(f"msg:  {msg}")
+        art = dynamodb.Table('art').get_item(
+            Key={'art_id': i})['Item']
+        try:
+            if 'name' in art['collection_data'] and art['collection_data']['name'] is not None:
+                if art['collection_data']['name'] in ['Mutant Ape Yacht Club',
+                                                      'Bored Ape Yacht Club, Bored Ape Kennel Club']:
+                    name = art['collection_data']['name']
+                    token_id = art['contractId#tokenId'].split('#')[1]
+                    name_split = name.split()
+                    hashtag = "#" + (name.replace(" ", ""))
+                    message = name_split[0] + " " + name_split[1] + " " + token_id + " sells for "
+                    usd_price = "${:,.2f}".format(
+                        round(((Decimal(art['last_sale_price']) / (10 ** 18)) / eth_rate), 2))
+                    tweet = message + usd_price + " " + art['buy_url'] + " " + hashtag + " #NFT #digitalart"
+                    msg = {
+                        "art_id": i,
+                        "message": tweet,
+                        "timestamp": str(datetime.utcnow().isoformat()),
+                        "platform": "twitter"
+                    }
+                    dynamodb.Table('auto_tweet').put_item(Item=msg)
+                    #. log.info(f"msg:  {msg}")
 
-                        return tweet
-                count += 1
-                art_list.append(art)
+                    return tweet
+            count += 1
+            art_list.append(art)
 
-            except Exception as e:
-                count += 1
-                log.info(e)
-                continue
+        except Exception as e:
+            count += 1
+            log.info(e)
+            continue
 
     for i in art_list:
-        log.info(i)
+        #. log.info(i)
         try:
             if i['name'] is not None or i['collection_data']['name'] is not None:
                 resp = dynamodb.Table('auto_tweet').get_item(
@@ -108,7 +107,7 @@ def get_art():
                     "platform": "twitter"
                 }
                 dynamodb.Table('auto_tweet').put_item(Item=msg)
-                log.info(f"msg:  {msg}")
+                #. log.info(f"msg:  {msg}")
 
                 return tweet
         except Exception as e:
