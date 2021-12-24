c4f06f73f53020d2b010b49682c6d151a2f6ee8c
theodorebrinkofski <tgb29@cornell.edu>
2021-12-24 15:26:20 -0600

    remove more

--- stack/sudocoins_stack/admin_api.py
+++ stack/sudocoins_stack/admin_api.py
@@ -24,46 +24,7 @@ class SudocoinsAdminApi:
             ),
             cors_preflight=cors_preflight
         )
-        # TRAFFIC REPORT
-        traffic_report_chart_data_integration = api_integrations.LambdaProxyIntegration(
-            handler=lambdas.traffic_report_chart_data_function
-        )
-        admin_api_v2.add_routes(
-            path='/traffic-report',
-            methods=[apigwv2.HttpMethod.GET],
-            integration=traffic_report_chart_data_integration,
-            authorizer=resources.sudocoins_admin_authorizer
-        )
-        # PAYOUTS
-        payouts_integration = api_integrations.LambdaProxyIntegration(
-            handler=lambdas.payouts_function
-        )
-        admin_api_v2.add_routes(
-            path='/payouts',
-            methods=[apigwv2.HttpMethod.GET],
-            integration=payouts_integration,
-            authorizer=resources.sudocoins_admin_authorizer
-        )
-        # USER DETAILS
-        user_details_integration = api_integrations.LambdaProxyIntegration(
-            handler=lambdas.user_details_function
-        )
-        admin_api_v2.add_routes(
-            path='/user/{userId}/details',
-            methods=[apigwv2.HttpMethod.GET],
-            integration=user_details_integration,
-            authorizer=resources.sudocoins_admin_authorizer
-        )
-        # UPDATE CASH OUT
-        update_cash_out_integration = api_integrations.LambdaProxyIntegration(
-            handler=lambdas.update_cash_out_function
-        )
-        admin_api_v2.add_routes(
-            path='/cash-out',
-            methods=[apigwv2.HttpMethod.POST],
-            integration=update_cash_out_integration,
-            authorizer=resources.sudocoins_admin_authorizer
-        )
+
         # GET PENDING UPCOMING
         pending_upcoming_integration = api_integrations.LambdaProxyIntegration(
             handler=lambdas.get_pending_upcoming_function
