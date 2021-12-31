bb3e8c1ef0db602ec63e672a8e565454464cbf38
theodorebrinkofski <tgb29@cornell.edu>
2021-12-30 18:00:28 -0600

    final sudo index

--- src/art/set_lists/sudo_index.py
+++ src/art/set_lists/sudo_index.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from decimal import Decimal, getcontext
 import pymysql
 import os
+from datetime import datetime
 
 log = sudocoins_logger.get()
 dynamodb = boto3.resource('dynamodb')
@@ -19,47 +20,46 @@ port = 3306
 
 
 def lambda_handler(event, context):
-    art = json.loads(event['Records'][0]['Sns']['Message'])
-    log.info(f'art: {art}')
+    collections = [3248, 1084]
 
-    collections = [1,2,3]
-
-    event_date = dynamodb.Table('Config').get_item(Key={'configKey': 'ingest2'})['Item']['last_update']
+    event_date = datetime.fromisoformat(
+        dynamodb.Table('Config').get_item(Key={'configKey': 'ingest2'})['Item']['last_update'])
     log.info(f'created: {event_date}')
 
-    log.info('about to connect')
     conn = pymysql.connect(host=rds_host, user=name, password=password, database=db_name, connect_timeout=15)
-    log.info('connection established')
     points = []
-    try:
-        with conn.cursor() as cur:
-            sql = '''select price from nft.events where price>0 and collection_id=%s and event_date >= %s - interval 7 day;'''
-            log.info(f'sql: {sql}')
 
-            for i in collections:
-                cur.execute(sql, (i, event_date))
-                result = cur.fetchall()
-                points.append[result]
+    with conn.cursor() as cur:
+        sql = '''select t.price from nft.events t inner join (select nft_id, max(event_date) as MaxDate from nft.events where price>0 and collection_id=%s and event_date >= %s - interval 14 day group by nft_id) tm on t.nft_id = tm.nft_id and t.event_date = tm.MaxDate where price>0;'''
+        log.info(f'sql: {sql}')
 
-        conn.close()
-
-        counter = 0
-        sum = 0
-
-        for j in points:
-            for k in j:
-                counter +=1
-                sum += k
-
-        sudo_index = sum / counter
-
-        with conn.cursor() as cur:
-            cur.execute(
-                'INSERT INTO `nft_events`.`open_sea_events` (`event_date`, `price`) VALUES (%s, %s)', (event_date,sudo_index))
-            conn.commit()
-            log.info("rds updated")
+        for i in collections:
+            cur.execute(sql, (i, event_date))
+            result = cur.fetchall()
+            log.info(result)
+            points.append(list(result))
 
         conn.close()
 
-    except Exception as e:
-        log.info(e)
+    counter = 0
+    sum = 0
+
+    for j in points:
+        for k in j:
+            counter += 1
+            sum += k[0]
+
+    sudo_index = int(sum / Decimal(counter))
+    log.info(f'sudo index: {sudo_index}')
+
+    conn = pymysql.connect(host=rds_host, user=name, password=password, database=db_name, connect_timeout=15)
+    with conn.cursor() as cur:
+        time_now = str(datetime.utcnow().isoformat())
+        row_values = (event_date, sudo_index)
+        cur.execute('INSERT INTO nft.sudo_index (`event_date`,`price`) VALUES (%s, %s)', row_values)
+
+        conn.commit()
+
+        log.info("rds updated")
+
+
