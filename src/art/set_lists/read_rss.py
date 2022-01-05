01093c2b0644f4dc48b021b02c9b0033c02578bd
theodorebrinkofski <tgb29@cornell.edu>
2022-01-04 19:11:12 -0600

    add media and description to the get_news response

--- src/art/set_lists/read_rss.py
+++ src/art/set_lists/read_rss.py
@@ -50,6 +50,10 @@ def add_news(news_results):
             msg['link'] = i.get('link')
             msg['title'] = i.get('title')
             msg['approved'] = 'true'
+            msg['source'] = 'cointelegraph'
+            msg['media'] = i.get('media')
+            msg['category'] = i.get('category')
+            msg['description'] = i.get('description')
 
             dynamodb.Table('news').put_item(Item=msg)
         except Exception as e:
@@ -64,14 +68,27 @@ def process_rss(rss_data):
     news_objects = []
     for i in news_list:
         datetime_object = datetime.strptime(i.pubDate.text, '%a, %d %b %Y %H:%M:%S +0000')
+
+        description = i.description.text
+        index = description.find('</p><p>')
+        description = description[index + 7:-17]
+
         msg = {
             "title": i.title.text,
             "pubDate": datetime_object,
-            "description": i.description.text,
+            "description": description,
             "link": i.link.text,
-            "media": i.find('media:content')
+            "media": i.find('content')['url']
         }
+
+        category_list = i.findAll('category')
+        categories = []
+        for i in category_list:
+            categories.append(str(i.contents[0]))
+        msg['category'] = categories
+
         # log.info(msg)
+
         news_objects.append(msg)
 
     return news_objects
