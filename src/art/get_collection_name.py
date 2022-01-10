a9c06641a4f2011f242e980a0631b1c7b27982e1
theodorebrinkofski <tgb29@cornell.edu>
2022-01-10 17:49:04 -0500

    change collection-name request so it can accept collection_url.

--- src/art/get_collection_name.py
+++ src/art/get_collection_name.py
@@ -13,42 +13,41 @@ def lambda_handler(event, context):
     set_log_context(event)
     body = json.loads(event['body'])
 
-    collection_id = body['collection_id']
+    collection_id = body.get('collection_id')
+    collection_url = body.get('collection_url')
+
 
     if collection_id == "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270:jiometory-no-compute---":
         collection_id = "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270:jiometory-no-compute---ジオメトリ-ハ-ケイサンサレマセン-by-samsy"
 
     try:
+        if collection_id is not None:
 
-        res = dynamodb.Table('collections').get_item(
-            Key={'collection_id': collection_id},
-            ProjectionExpression="collection_data, sales_volume, collection_date, sale_count, floor, median, maximum, more_charts, open_sea, blockchain")
+            res = dynamodb.Table('collections').get_item(
+                Key={'collection_id': collection_id},
+                ProjectionExpression="collection_data, sales_volume, collection_date, sale_count, floor, median, maximum, more_charts, open_sea, blockchain")
 
-        if 'Item' in res and 'collection_data' in res['Item']:
-            return res['Item']
+            if 'Item' in res and 'collection_data' in res['Item']:
+                return res['Item']
+
+        else:
+            data = dynamodb.Table('art').query(
+                KeyConditionExpression=Key('collection_url').eq(collection_url),
+                ScanIndexForward=False,
+                Limit=1,
+                IndexName='collection_url-index',
+                ProjectionExpression='collection_data, sales_volume, collection_date, sale_count, floor, median, maximum, more_charts, open_sea, blockchain'
+            )
+
+            collection_data = data['Items'][0]
+
+            return collection_data
 
     except Exception as e:
-        log.info(e)
-
-    return {
-        'collection_data': get_collection(collection_id)['collection_data']
-    }
+        log.info(f"status: failure - {e}")
 
 
 def set_log_context(event):
     global log
     log = sudocoins_logger.get(sudocoins_logger.get_ctx(event))
 
-
-def get_collection(collection_id):
-    data = dynamodb.Table('art').query(
-        KeyConditionExpression=Key('collection_id').eq(collection_id),
-        ScanIndexForward=False,
-        Limit=1,
-        IndexName='collection_id-event_date-index',
-        ProjectionExpression='collection_data, sales_volume, collection_date, sale_count, floor, median, maximum, blockchain'
-    )
-
-    collection_data = data['Items'][0]
-
-    return collection_data
